fb04486946af42cb2da1188c8a5bbfe7bf18fb32
Jisu Lee <jisulee.shsf@gmail.com>
2023-03-27 00:19:00 +0900

    Upload file

---
+++ chap_03/_01_string1_230327.java
@@ -0,0 +1,48 @@
+package chap_03;
+
+public class _01_string1_230327 {
+    public static void main(String[] args) {
+        String s = "I like Java and Python and C.";
+        System.out.println(s); // I like Java and Python and C.
+
+        // 1. length(): 문자열 길이 확인
+        System.out.println(s.length()); // 29
+
+        // 2. contains(): 특정 문자열 포함 여부 확인
+        System.out.println(s.contains("Java")); // true
+        System.out.println(s.contains("JAVA")); // false
+        System.out.println(s.contains("C#")); // false
+
+        // 3. startsWith() & endsWith: 특정 문자열로 시작 ・ 끝나는지 여부 확인
+        System.out.println(s.startsWith("I")); // true
+        System.out.println(s.endsWith("C")); // false
+
+        // 3.indexOf(): 특정 문자열이 시작되는 위치 확인
+        System.out.println(s.indexOf("Java")); // 7
+        System.out.println(s.indexOf("C#")); // -1
+
+        // 5. toUpperCase() & toLowerCase(): 문자열 대소문자 변환
+        System.out.println(s.toUpperCase()); // I LIKE JAVA AND PYTHON AND C.
+        System.out.println(s.toLowerCase()); // i like java and python and c.
+
+        // 6. replace(): 문자열 치환
+        System.out.println(s.replace("C", "C#")); // I like Java and Python and C#.
+
+        // 7. substring() 문자열 자르기
+        System.out.println(s.substring(s.indexOf("Java"))); // Java and Python and C.
+        System.out.println(s.substring(s.indexOf("Java"), s.indexOf("."))); // Java and Python and C
+
+        // 8. concat(): 문자열 결합
+        String s1 = "Java";
+        String s2 = "Python";
+        System.out.println(s1 + s2); // JavaPython
+        System.out.println(s1 + ", " + s2); // Java, Python
+        System.out.println(s1.concat(s2)); // JavaPython
+        System.out.println(s1.concat(", ").concat(s2)); // Java, Python
+
+        // 9. trim(): 문자열 앞뒤 공백 제거
+        String s3 = "     I like Java.     ";
+        System.out.println(s3); //      I like Java.
+        System.out.println(s3.trim()); // I like Java.
+    }
+}
