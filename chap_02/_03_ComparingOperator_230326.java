a6d7c5cbea3a6df10020301048ea77ab8ad92f07
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2023-03-27 02:17:16 +0900

    Update and rename _03_comparingOperator_230326.java to _03_ComparingOperator_230326.java

--- chap_02/_03_ComparingOperator_230326.java
+++ chap_02/_03_ComparingOperator_230326.java
@@ -1,6 +1,6 @@
 package chap_02;
 
-public class _03_comparingOperator_230326 {
+public class _03_ComparingOperator_230326 {
     public static void main(String[] args) {
         // 비교 연산자
         System.out.println(3 > 1); // true
