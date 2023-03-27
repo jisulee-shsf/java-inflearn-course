7fb9883979c94a252177294b8edde14f2a06284c
Jisu Lee <jisulee.shsf@gmail.com>
2023-03-28 01:21:43 +0900

    Upload file

---
+++ chap_04/_01_If1_230328.java
@@ -0,0 +1,52 @@
+package chap_04;
+
+public class _01_If1_230328 {
+    public static void main(String[] args) {
+        // 1-1. if문의 조건이 참인 경우 & {}를 생략한 경우
+        int hour = 6;
+        if (hour == 6)
+            System.out.println("PT를 시작합니다.");
+            System.out.println("PT를 진행 중입니다.");
+        System.out.println("PT를 종료합니다. #1");
+        /*
+        PT를 시작합니다.
+        PT를 진행 중입니다.
+        PT를 종료합니다. #1
+        */
+
+        // 1-2. if문의 조건이 참인 경우 & {}를 표기한 경우
+        hour = 6;
+        if (hour == 6) {
+            System.out.println("PT를 시작합니다.");
+            System.out.println("PT를 진행 중입니다.");
+        }
+        System.out.println("PT를 종료합니다. #2");
+        /*
+        PT를 시작합니다.
+        PT를 진행 중입니다.
+        PT를 종료합니다. #2
+        */
+
+        // 2-1. if문의 조건이 거짓인 경우 & {}를 생략한 경우
+        hour = 7;
+        if (hour == 6)
+            System.out.println("PT를 시작합니다.");
+            System.out.println("PT를 진행 중입니다.");
+        System.out.println("PT를 종료합니다. #3");
+        /*
+        PT를 진행 중입니다.
+        PT를 종료합니다. #3
+        */
+
+        // 2-2. if문의 조건이 거짓인 경우 & {}를 표기한 경우
+        hour = 7;
+        if (hour == 6) {
+            System.out.println("PT를 시작합니다.");
+            System.out.println("PT를 진행 중입니다.");
+        }
+        System.out.println("PT를 종료합니다. #4");
+        /*
+        PT를 종료합니다. #4
+        */
+    }
+}
