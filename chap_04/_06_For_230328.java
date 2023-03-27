f840d7e20797fe780bbda8386713c9d8ef5e8e3a
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2023-03-28 01:54:05 +0900

    Update _06_For_230328.java

--- chap_04/_06_For_230328.java
+++ chap_04/_06_For_230328.java
@@ -9,28 +9,25 @@ public class _06_For_230328 {
 
         System.out.println();
 
-        // 2-1. 짝수만 출력
+        // 2-1. 짝수 ・ 홀수 출력
         for (int i = 0; i < 10; i += 2) {
             System.out.print(i); // 02468
         }
 
-        System.out.println();
-
-        // 2-2. 홀수만 출력
         for (int i = 1; i < 10; i += 2) {
             System.out.print(i); //13579
         }
 
         System.out.println();
 
-        // 2-3. 숫자 거꾸로 출력
+        // 2-2. 숫자 거꾸로 출력
         for (int i = 10; i > 0; i--) {
             System.out.print(i); //10987654321
         }
 
         System.out.println();
 
-        // 2-4. 1부터 10까지 총합 출력
+        // 2-3. 숫자 총합 출력
         int sum = 0;
         for (int i = 1; i < 11; i++) {
             System.out.print(i); // 12345678910
