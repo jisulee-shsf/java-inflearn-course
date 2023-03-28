82904576f5e1081ca3c19b664ca44bb0d1f9bcbd
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2023-03-28 17:30:20 +0900

    Update _07_While_230328.java

--- chap_04/_07_While_230328.java
+++ chap_04/_07_While_230328.java
@@ -2,27 +2,27 @@ package chap_04;
 
 public class _07_While_230328 {
     public static void main(String[] args) {
-        // 1. while문
+        // 1-1. while문
         int height = 10;
         int move = 3;
         while (move < height) {
             System.out.println("현재 위치는 " + move + "입니다.");
             move += 3;
         }
-        System.out.println(move + "로 이동이 종료되었습니다. #1");
+        System.out.println(move + "로 이동이 종료되었습니다. #1-1");
         /*
         현재 위치는 3입니다.
         현재 위치는 6입니다.
         현재 위치는 9입니다.
-        12로 이동이 종료되었습니다. #1
+        12로 이동이 종료되었습니다. #1-1
         */
 
-        // 1-1. 무한 루프
+        // 1-2. 무한 루프
         move = 0;
         while (move < height) {
             System.out.println("현재 위치는 " + move + "입니다.");
         }
-        System.out.println("이동이 종료되었습니다. #2");
+        System.out.println("이동이 종료되었습니다. #1-2");
         /*
         현재 위치는 0입니다.
         현재 위치는 0입니다.
@@ -37,12 +37,12 @@ public class _07_While_230328 {
             System.out.println("현재 위치는 " + move + "입니다.");
             move += 3;
         }
-        System.out.println(move + "로 이동이 종료되었습니다. #3");
+        System.out.println(move + "로 이동이 종료되었습니다. #2-1");
         /*
         현재 위치는 0입니다.
         현재 위치는 3입니다.
         현재 위치는 6입니다.
-        9로 이동이 종료되었습니다. #3
+        9로 이동이 종료되었습니다. #2-1
         */
 
         move = 0;
@@ -51,19 +51,19 @@ public class _07_While_230328 {
             System.out.println("현재 위치는 " + move + "입니다.");
             move += 3;
         }
-        System.out.println(move + "로 이동이 종료되었습니다. #4");
+        System.out.println(move + "로 이동이 종료되었습니다. #2-2");
         /*
-        0로 이동이 종료되었습니다. #4
+        0로 이동이 종료되었습니다. #2-2
         */
 
         do {
             System.out.println("현재 위치는 " + move + "입니다.");
             move += 3;
         } while (move + step < height);
-        System.out.println(move + "로 이동이 종료되었습니다. #5");
+        System.out.println(move + "로 이동이 종료되었습니다. #2-3");
         /*
         현재 위치는 0입니다.
-        3로 이동이 종료되었습니다. #5
+        3로 이동이 종료되었습니다. #2-3
         */
     }
 }
