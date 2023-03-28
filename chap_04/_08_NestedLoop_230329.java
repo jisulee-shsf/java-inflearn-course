c3d17bd7da4e37b6ca78cbd0d3d260c93b6ff11f
Jisu Lee <jisulee.shsf@gmail.com>
2023-03-29 01:10:03 +0900

    Upload file

---
+++ chap_04/_08_NestedLoop_230329.java
@@ -0,0 +1,99 @@
+package chap_04;
+
+public class _08_NestedLoop_230329 {
+    public static void main(String[] args) {
+        // 1. * 사각형 만들기
+        for (int i = 0; i < 5; i++) {
+            System.out.print("*");
+            System.out.print("*");
+            System.out.print("*");
+            System.out.print("*");
+            System.out.print("*");
+            System.out.println();
+        }
+        System.out.println("#1-1");
+        /*
+        *****
+        *****
+        *****
+        *****
+        *****
+        #1-1
+        */
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println("#1-2");
+        /*
+        *****
+        *****
+        *****
+        *****
+        *****
+        #1-2
+        */
+
+        // 2. * 좌우 삼각형 만들기
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                // for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println("#2-1");
+        /*
+        * -> i = 0 / j = 0
+        ** -> i = 1 / j = 0, 1
+        *** -> i = 2 / j = 0, 1, 2
+        **** -> i = 3 / j = 0, 1, 2, 3
+        ***** -> i = 4 / j = 0, 1, 2, 3, 4
+        #2-1
+        */
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 4 - i; j++) {
+                System.out.print(" ");
+                /*
+                1) i = 0,  j = 0
+                   0 < (4 - 0) -> s
+                   1 < (4 - 0) -> s
+                   2 < (4 - 0) -> s
+                   3 < (4 - 0) -> s
+                   4 < (4 - 0) -> X
+                2) i = 1,  j = 0
+                   0 < (4 - 1) -> s
+                   1 < (4 - 1) -> s
+                   2 < (4 - 1) -> s
+                   3 < (4 - 1) -> X
+                3) i = 2,  j = 0
+                   0 < (4 - 2) -> s
+                   1 < (4 - 3) -> s
+                   2 < (4 - 4) -> X
+                3) i = 3,  j = 0
+                   0 < (4 - 3) -> s
+                   1 < (4 - 3) -> X
+                4) i = 4,  j = 0
+                   0 < (4 - 4) -> X
+                */
+            }
+            for (int k = 0; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println("#2-2");
+        /*
+            *
+           **
+          ***
+         ****
+        *****
+        #2-2
+        */
+    }
+}
