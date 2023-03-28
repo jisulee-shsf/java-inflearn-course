bfc21909564a583ffc0581f6a1405e21f8b2cde8
Jisu Lee <jisulee.shsf@gmail.com>
2023-03-28 16:43:51 +0900

    Upload file

---
+++ chap_04/_07_While_230328.java
@@ -0,0 +1,33 @@
+package chap_04;
+
+public class _07_While_230328 {
+    public static void main(String[] args) {
+        // 1. while문
+        int height = 10;
+        int move = 3;
+        while (move < height) {
+            System.out.println("현재 위치는 " + move + "입니다.");
+            move += 3;
+        }
+        System.out.println(move + "로 이동이 종료되었습니다. #1");
+        /*
+        현재 위치는 3입니다.
+        현재 위치는 6입니다.
+        현재 위치는 9입니다.
+        12로 이동이 종료되었습니다. #1
+        */
+
+        // 2. 무한 루프
+        move = 0;
+        while (move < height) {
+            System.out.println("현재 위치는 " + move + "입니다.");
+        }
+        System.out.println("이동이 종료되었습니다. #2");
+        /*
+        현재 위치는 0입니다.
+        현재 위치는 0입니다.
+        현재 위치는 0입니다.
+        ...
+        */
+    }
+}
