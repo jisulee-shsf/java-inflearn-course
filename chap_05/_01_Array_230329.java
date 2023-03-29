ebe35789ca6cdb90bca6d6257f964c5a53993ba0
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2023-03-29 19:27:42 +0900

    Update _01_Array_230329.java

--- chap_05/_01_Array_230329.java
+++ chap_05/_01_Array_230329.java
@@ -32,19 +32,6 @@ public class _01_Array_230329 {
         // 2.2. array를 사용한 경우
         System.out.println("A는 " + coffees4[0] + " 한 잔");
         System.out.println("B는 " + coffees4[1] + " 한 잔");
-        System.out.println("C는 " + coffees4[2] + " 한 잔");
-        System.out.println("주세요.");
-        System.out.println("주문 완료 #2");
-        /*
-        A는 americano 한 잔
-        B는 latte 한 잔
-        C는 mocha 한 잔
-        주세요.
-        주문 완료 #2
-        */
-
-        System.out.println("A는 " + coffees4[0] + " 한 잔");
-        System.out.println("B는 " + coffees4[1] + " 한 잔");
         coffees4[2] = "Cappuccino";
         System.out.println("C는 " + coffees4[2] + " 한 잔");
         System.out.println("주세요.");
