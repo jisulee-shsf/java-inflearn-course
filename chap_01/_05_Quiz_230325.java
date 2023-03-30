8b5f269fd89533c77e4ea1536ce3cc7ab0203936
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2023-03-31 05:25:19 +0900

    Update _05_Quiz_230325.java

--- chap_01/_05_Quiz_230325.java
+++ chap_01/_05_Quiz_230325.java
@@ -1,8 +1,9 @@
 package chap_01;
 
-// Q. 버스 도착 정보를 출력하는 프로그램 작성하기
+// 버스 도착 정보를 출력하는 프로그램 작성
 
 /*
+예시)
 신당03번 버스
 약 3분 후 도착
 남은 거리 1.3km
@@ -10,16 +11,13 @@ package chap_01;
 
 public class _05_Quiz_230325 {
     public static void main(String[] args) {
-        // 신당03번 버스
         String busNo = "신당03";
-        System.out.println(busNo + "번 버스");
+        System.out.println(busNo + "번 버스"); // 신당03번 버스
         
-        // 약 3분 후 도착
         int time = 3;
-        System.out.println("약 " + time + "분 후 도착");
+        System.out.println("약 " + time + "분 후 도착"); // 약 3분 후 도착
         
-        // 남은 거리 1.3km
         float distance = 1.3F;
-        System.out.println("남은 거리 " + distance + "km");
+        System.out.println("남은 거리 " + distance + "km"); // // 남은 거리 1.3km
     }
 }
