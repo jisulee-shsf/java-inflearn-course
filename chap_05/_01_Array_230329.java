0c723f43e9af0f53308693c313474aca81c95257
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2023-03-31 04:20:44 +0900

    Update _01_Array_230329.java

--- chap_05/_01_Array_230329.java
+++ chap_05/_01_Array_230329.java
@@ -2,13 +2,14 @@ package chap_05;
 
 public class _01_Array_230329 {
     public static void main(String[] args) {
-        // 1. array를 사용하지 않은 경우
-        String coffeA = "americano";
-        String coffeB = "latte";
-        String coffeC = "mocha";
-        System.out.println("A는 " + coffeA + " 한 잔");
-        System.out.println("B는 " + coffeB + " 한 잔");
-        System.out.println("C는 " + coffeC + " 한 잔");
+        // 1. 배열을 사용하지 않은 경우
+        String coffee1 = "americano";
+        String coffee2 = "latte";
+        String coffee3 = "mocha";
+        
+        System.out.println("A는 " + coffee1 + " 한 잔");
+        System.out.println("B는 " + coffee2 + " 한 잔");
+        System.out.println("C는 " + coffee3 + " 한 잔");
         System.out.println("주세요.");
         System.out.println("주문 완료 #1");
         /*
@@ -19,20 +20,24 @@ public class _01_Array_230329 {
         주문 완료 #1
         */
 
-        // 2-1. array 선언
-        String[] coffees1 = new String[3]; // String coffees2[] = new String[3];
-        coffees1[0] = "americano"; // A
-        coffees1[1] = "latte"; // B
-        coffees1[2] = "mocha"; // C
+        // 2. 배열을 사용한 경우
+        String[] coffees1 = new String[3];
+        coffees1[0] = "americano";
+        coffees1[1] = "latte";
+        coffees1[2] = "mocha";
+        
+        String coffees2[] = new String[3];
+        coffees2[0] = "americano";
+        coffees2[1] = "latte";
+        coffees3[2] = "mocha";
 
         String[] coffees3 = new String[] {"americano", "latte", "mocha"};
 
         String[] coffees4 = {"americano", "latte", "mocha"};
-
-        // 2.2. array를 사용한 경우
+        
         System.out.println("A는 " + coffees4[0] + " 한 잔");
         System.out.println("B는 " + coffees4[1] + " 한 잔");
-        coffees4[2] = "Cappuccino";
+        coffees4[2] = "Cappuccino"; // 값 업데이트 
         System.out.println("C는 " + coffees4[2] + " 한 잔");
         System.out.println("주세요.");
         System.out.println("주문 완료 #2");
@@ -44,7 +49,7 @@ public class _01_Array_230329 {
         주문 완료 #2
         */
 
-        // 3. String 외, 다른 자료형에 array를 사용한 경우
+        // 3. String 외, 다른 자료형에 배열을 사용한 경우
         int[] test1 = {10, 20, 30};
         System.out.println(test1[0]); // 10
 
