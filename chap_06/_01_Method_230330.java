f9d89d394902f55dfd5c07d3550aa83b3fcef6a2
Jisu Lee <jisulee.shsf@gmail.com>
2023-03-30 14:34:08 +0900

    Upload file

---
+++ chap_06/_01_Method_230330.java
@@ -0,0 +1,20 @@
+package chap_06;
+
+public class _01_Method_230330 {
+    // 1. 메소드 정의
+    public static void sayHello() {
+        System.out.println("메소드입니다.");
+    }
+
+    // 2. 메소드 호출
+    public static void main(String[] args) {
+        System.out.println("메소드 호출 전입니다.");
+        sayHello();
+        System.out.println("메소드 호출 후입니다.");
+        /*
+        메소드 호출 전입니다.
+        메소드입니다.
+        메소드 호출 후입니다.
+        */
+    }
+}
