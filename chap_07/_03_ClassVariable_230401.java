4d3fab099cb16d76f10c673fd7c091730e82776b
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2023-04-01 05:08:14 +0900

    Update _03_ClassVariable_230401.java

--- chap_07/_03_ClassVariable_230401.java
+++ chap_07/_03_ClassVariable_230401.java
@@ -4,14 +4,14 @@ public class _03_ClassVariable_230401 {
     public static void main(String[] args) {
         // 1. 클래스 변수 사용
         Phone p = new Phone();
-        p.modelName = "나도폰";
-        System.out.println(p.modelName); // 나도폰
+        p.modelName = "자바폰";
+        System.out.println(p.modelName); // 자바폰
         // System.out.println(p.canCrashDetection); // false
         System.out.println(Phone.canCrashDetection); // false
 
         // 2. 클래스 변수 값 변경
         Phone.canCrashDetection = true;
-        // System.out.println(p.canCrashDetection); // false
+        // System.out.println(p.canCrashDetection); // true
         System.out.println(Phone.canCrashDetection); // true
     }
 }
