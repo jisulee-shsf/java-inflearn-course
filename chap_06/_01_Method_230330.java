9b4f22094200d1451530ddd5e90c936a5e012572
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2023-04-01 05:51:51 +0900

    Update _01_Method_230330.java

--- chap_06/_01_Method_230330.java
+++ chap_06/_01_Method_230330.java
@@ -1,20 +1,20 @@
 package chap_06;
 
 public class _01_Method_230330 {
-    // 1. 메소드 정의
+    // 1. 메서드 정의
     public static void sayHello() {
-        System.out.println("메소드입니다.");
+        System.out.println("메서드입니다."); // 메서드입니다.
     }
 
-    // 2. 메소드 호출
+    // 2. 메서드 호출
     public static void main(String[] args) {
-        System.out.println("메소드 호출 전입니다.");
+        System.out.println("메서드 호출 전입니다.");
         sayHello();
-        System.out.println("메소드 호출 후입니다.");
+        System.out.println("메서드 호출 후입니다.");
         /*
-        메소드 호출 전입니다.
-        메소드입니다.
-        메소드 호출 후입니다.
+        메서드 호출 전입니다.
+        메서드입니다.
+        메서드 호출 후입니다.
         */
     }
 }
