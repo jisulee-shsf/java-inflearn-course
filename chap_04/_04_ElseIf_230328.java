b8bec9e4d224bdf7d9631734c0fc887e142922d3
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2023-03-31 17:26:42 +0900

    Update _04_ElseIf_230328.java

--- chap_04/_04_ElseIf_230328.java
+++ chap_04/_04_ElseIf_230328.java
@@ -2,16 +2,16 @@ package chap_04;
 
 public class _04_ElseIf_230328 {
     public static void main(String[] args) {
-        // else if문
+        // if-else if문
         boolean latte = false;
         boolean mocha = true;
-        if (latte) { // if문의 조건 거짓 -> else if문 이동
+        if (latte) { // false -> else if문 이동
             System.out.println("latte를 주문하셨습니다.");
             System.out.println("음료를 제조하고 있습니다. #1");
-        } else if (mocha) { // else if문의 조건 참 -> 문장 출력
+        } else if (mocha) { // true -> 수행
             System.out.println("mocha를 주문하셨습니다.");
             System.out.println("음료를 제조하고 있습니다. #2");
-        } else { // else if문의 조건이 거짓일 경우, else문 문장 출력
+        } else {
             System.out.println("latte와 mocha가 모두 소진되어 americano를 주문하셨습니다.");
             System.out.println("음료를 제조하고 있습니다. #3");
         }
