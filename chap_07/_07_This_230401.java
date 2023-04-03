f2cd2f9f424ef8265c5f9fbf291a20837b48cf23
Jisu Lee <jisulee.shsf@gmail.com>
2023-04-03 20:15:43 +0900

    Update _07_This_230401

--- chap_07/_07_This_230401.java
+++ chap_07/_07_This_230401.java
@@ -4,13 +4,26 @@ public class _07_This_230401 {
     public static void main(String[] args) {
         // 1. 클래스 객체 생성
         Phone p = new Phone();
-        p.modelName = "자바폰";
+
+        // 2. 인스턴스 변수 값 설정
         p.modelType = "프로";
 
-        // 2. 참조변수 this를 사용한 인스턴스 변수 값 변경
-        // p.appendModelType("맥스"); // 맥스 += 맥스
-        // System.out.println(p.modelType); 맥스
+        // 3-1. 메서드 호출(modelType += modelType;)
+        p.appendModelType("맥스");
+        System.out.println(p.modelType); // 맥스
+
+        // 3-2. 메서드 호출(this.modelType += modelType;)
         p.appendModelType("맥스");
         System.out.println(p.modelType); // 프로맥스
     }
 }
+
+/*
+// 참조변수 this
+public class Phone {
+    void appendModelType(String modelType) {
+        // modelType += modelType; // 매개변수 modelType += 매개변수 modelType -> 맥스 += 맥스
+        this.modelType += modelType; // 인스턴스 변수 modelType += 매개변수 modelType -> modelType += 맥스
+    }
+}
+*/
