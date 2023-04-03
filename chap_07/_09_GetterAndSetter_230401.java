4b43587f5110aa76187f683e459c2fc52050961a
Jisu Lee <jisulee.shsf@gmail.com>
2023-04-04 01:48:38 +0900

    Update _09_GetterAndSetter_230401

--- chap_07/_09_GetterAndSetter_230401.java
+++ chap_07/_09_GetterAndSetter_230401.java
@@ -5,12 +5,13 @@ public class _09_GetterAndSetter_230401 {
         // 1. 클래스 객체 생성
         Phone p = new Phone();
 
-        // 2. Getter & Setter 사용
+        // 2-1. 메서드 호출(setter)
         p.setModelName("자바폰");
-        // p.setModelType("프로맥스"); -> 인스턴스 변수 값 누락
+        // p.setModelType("프로맥스"); 인스턴스 변수 값 누락
         p.setModelCapacity(120); // -> 용량 정보 오기입
         p.setModelPrice(1000000);
 
+        // 2-2. 메서드 호출(getter)
         System.out.println("모델명: " + p.getModelName());
         System.out.println("모델 타입: " + p.getModelType());
         System.out.println("모델 용량: " + p.getModelCapacity());
@@ -23,3 +24,33 @@ public class _09_GetterAndSetter_230401 {
         */
     }
 }
+
+/*
+// getter & setter
+public class Phone {
+    // 1. modelType 인스턴스 변수의 getter & setter 메서드 정의
+    String getModelType() {
+        if (modelType == null || modelType.isEmpty()) {
+            return "오류가 발생했습니다.";
+        }
+        return modelType;
+    }
+
+    void setModelType(String modelType) {
+        this.modelType = modelType;
+    }
+
+    // 2. modelCapacity 인스턴스 변수의 getter & setter 메서드 정의
+    int getModelCapacity() {
+        return modelCapacity;
+    }
+
+    void setModelCapacity(int modelCapacity) {
+        if (modelCapacity < 128) {
+            this.modelCapacity = 128;
+        } else {
+            this.modelCapacity = modelCapacity;
+        }
+    }
+}
+*/
