3aa7b98032081e3ccf41d310244a336ed54e8512
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2023-04-03 16:53:20 +0900

    Update _01_Class_230401.java

--- chap_07/_01_Class_230401.java
+++ chap_07/_01_Class_230401.java
@@ -3,16 +3,16 @@ package chap_07;
 public class _01_Class_230401 {
     public static void main(String[] args) {
         // 1. 기존 데이터 및 함수 생성 방식
-        String modelName = "자바폰1";
-        String modelType = "프로";
-        int modelCapacity = 128;
-        int modelPrice = 1000000;
+        String modelName1 = "자바폰1";
+        String modelType1 = "프로";
+        int modelCapacity1 = 128;
+        int modelPrice1 = 1000000;
         
-        String modelName = "자바폰2";
-        String modelType = "프로맥스";
-        int modelCapacity = 256;
-        int modelPrice = 1500000;
-        ...   
+        String modelName2 = "자바폰2";
+        String modelType2 = "프로맥스";
+        int modelCapacity2 = 256;
+        int modelPrice2 = 1500000;
+        // ...   
 
         // 2. 클래스 객체 생성
         Phone p1 = new Phone(); // Phone 클래스의 p1 객체 생성
