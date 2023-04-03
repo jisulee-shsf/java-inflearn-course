e7a48fcb26e89238a44127d3b387173c7819c1e5
Jisu Lee <jisulee.shsf@gmail.com>
2023-04-03 18:17:14 +0900

    Update _04_Method_230401

--- chap_07/_04_Method_230401.java
+++ chap_07/_04_Method_230401.java
@@ -4,11 +4,43 @@ public class _04_Method_230401 {
     public static void main(String[] args) {
         // 1. 클래스 객체 생성
         Phone p = new Phone();
+
+        // 2. 인스턴스 변수 값 설정
         p.modelName = "자바폰";
         System.out.println(p.modelName); // 자바폰
 
         // 2. 메서드 호출
-        int objectCount = p.crashDetection(1);
-        System.out.println("충돌이 감지된 물체 개수: " + objectCount + "개"); // 충돌이 감지된 물체 개수: 10개
+        p.canCrashDetection1(); // 충돌이 감지되었습니다.
+        p.canCrashDetection2(1); // 충돌이 감지되었습니다.
+        int objectCount = p.crashDetection3(1);
+        System.out.println(p.modelName + "에 충돌이 감지된 물체 개수: " + objectCount + "개"); // 자바폰에 충돌이 감지된 물체 개수: 10개
     }
 }
+
+/*
+public class Phone {
+    // 1. 메서드 정의
+    // 1) 매개변수와 리턴값이 없는 기본 메서드
+    void canCrashDetection1() {
+        System.out.println("충돌이 감지되었습니다.");
+    }
+
+    // 2) 매개변수가 있는 메서드
+    void canCrashDetection2(int number) {
+        if (number == 1) {
+            System.out.println("충돌이 감지되었습니다.");
+        } else {
+            System.out.println("충돌이 감지되지 않았습니다.");
+        }
+    }
+
+    // 3) 매개변수와 리턴값이 있는 메서드
+    int crashDetection3(int number) {
+        if (number == 1) { // 충돌이 감지된 경우
+            return 10; // 충돌 감지 물체 개수
+        } else { // 이외의 경우
+            return 0; // 충돌 감지 물체 없음
+        }
+    }
+}
+*/
