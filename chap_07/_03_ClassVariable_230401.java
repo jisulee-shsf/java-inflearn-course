2ced24c1a4b2420346e5e37d39a5f2f48963377c
Jisu Lee <jisulee.shsf@gmail.com>
2023-04-03 17:39:27 +0900

    Update _03_ClassVariable_230401

--- chap_07/_03_ClassVariable_230401.java
+++ chap_07/_03_ClassVariable_230401.java
@@ -2,16 +2,27 @@ package chap_07;
 
 public class _03_ClassVariable_230401 {
     public static void main(String[] args) {
-        // 1. 클래스 변수 사용
+        // 1. 클래스 객체 생성
         Phone p = new Phone();
+
+        // 2. 인스턴스 변수 값 설정
         p.modelName = "자바폰";
         System.out.println(p.modelName); // 자바폰
-        // System.out.println(p.canCrashDetection); // false
-        System.out.println(Phone.canCrashDetection); // false
 
-        // 2. 클래스 변수 값 변경
+        // 3. 클래스 변수 사용
+        System.out.println(p.canCrashDetection); // 객체 -> false
+        System.out.println(Phone.canCrashDetection); // 인스턴스 전체 -> false
+
+        // 4. 클래스 변수 값 변경
         Phone.canCrashDetection = true;
-        // System.out.println(p.canCrashDetection); // true
-        System.out.println(Phone.canCrashDetection); // true
+        System.out.println(p.canCrashDetection); // 객체 -> true
+        System.out.println(Phone.canCrashDetection); // 인스턴스 전체 -> true
     }
 }
+
+/*
+public class Phone {
+    // 1. 클래스 변수 선언(static)
+    static boolean canCrashDetection = false;
+}
+*/
