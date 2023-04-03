6c77b52fae41dad39afd824ad1f8a2fecd0c03f2
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2023-04-03 18:20:54 +0900

    Update _03_Return_230330.java

--- chap_06/_03_Return_230330.java
+++ chap_06/_03_Return_230330.java
@@ -1,7 +1,7 @@
 package chap_06;
 
 public class _03_Return_230330 {
-    // 1. 메서드 정의(리턴 포함)
+    // 1. 메서드 정의
     public static String getAddress() {
         String address = "서울시 중구 000";
         return address;
@@ -16,12 +16,12 @@ public class _03_Return_230330 {
         return "02-0000-0000";
     }
 
-    // 2. 메드 호출
+    // 2. 메서드 호출
     public static void main(String[] args) {
-        String officeAddress = getAddress(); // 리턴값을 저장하기 위한 변수 정의
+        String officeAddress = getAddress(); // 반환값을 저장하기 위한 변수 정의
         System.out.println("사무실 주소: " + officeAddress); // 사무실 주소: 서울시 중구 000
 
-        int officeZipCode = getZipCode(); // 리턴값을 저장하기 위한 변수 정의
+        int officeZipCode = getZipCode(); // 반환값을 저장하기 위한 변수 정의
         System.out.println("사무실 우편번호: " + officeZipCode); // 사무실 우편번호: 12345
 
         System.out.println("사무실 전화번호: " + getNumber()); // 사무실 전화번호: 02-0000-0000
