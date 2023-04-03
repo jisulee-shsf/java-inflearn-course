fc5a47213cbef23b7b09691571ac87e828d12748
Jisu Lee <jisulee.shsf@gmail.com>
2023-04-04 02:54:03 +0900

    Update _10_AccessModifier_230402

--- chap_07/_10_AccessModifier_230402.java
+++ chap_07/_10_AccessModifier_230402.java
@@ -13,7 +13,7 @@ public class _10_AccessModifier_230402 {
         pu.modelPrice = -1000000; // 잘못된 값으로 설정될 수 있음
         System.out.println(pu.modelPrice); // -1000000
 
-        // 3. 접근 제어자 private 사용
+        // 3. 접근 제어자 변경(public -> private) 후, 인스턴스 변수 값 재설정
         // java: modelPrice has private access in chap_07.PhoneUpdated
         pu.modelPrice = 1000000;
         System.out.println(pu.modelPrice);
@@ -22,8 +22,23 @@ public class _10_AccessModifier_230402 {
         System.out.println(pu.modelPrice);
         */
 
-        // 4. Getter & Setter 접근
+        // 4. 메서드 호출(getter & setter)
         pu.setModelPrice(1500000);
         System.out.println(pu.getModelPrice()); // 1500000
     }
 }
+
+/*
+public class PhoneUpdated {
+    // 1. 인스턴스 변수 선언
+    private int modelPrice; // 접근 제어자 변경(public -> private)
+
+    // 2. 메서드 정의(getter & setter)
+    public int getModelPrice() {
+        return modelPrice;
+    }
+    public void setModelPrice(int modelPrice) {
+        this.modelPrice = modelPrice;
+    }
+}
+*/
