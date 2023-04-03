99a40eb49cd562abafd925196e7a0b42a36834b7
Jisu Lee <jisulee.shsf@gmail.com>
2023-04-03 22:09:51 +0900

    Update _08_Constructor_230401

--- chap_07/_08_Constructor_230401.java
+++ chap_07/_08_Constructor_230401.java
@@ -6,18 +6,50 @@ public class _08_Constructor_230401 {
         Phone p1 = new Phone();
         /*
         기본 생성자 호출입니다.
+        1번의 모델 번호를 발급했습니다.
         */
 
-        // 2. 기본 생성자 및 사용자 정의 생성자 호출
+        // 2. 사용자 정의 생성자 호출(this())
         Phone p2 = new Phone("자바폰", "프로", 128, 1000000);
         System.out.println(p2.modelName);
+        System.out.println(p2.modelType);
+        System.out.println(p2.modelCapacity);
+        System.out.println(p2.modelPrice);
         System.out.println(p2.modelNumber);
         /*
-        기본 생성자 호출입니다. -> this()
+        기본 생성자 호출입니다.
         2번의 모델 번호를 발급했습니다.
         사용자 정의 생성자 호출입니다.
         자바폰
+        프로
+        128
+        1000000
         2
         */
     }
 }
+
+/*
+// 생성자
+public class Phone {
+    // 1. 클래식 변수 정의
+    static int modelNumberCounter = 0;
+
+    // 2. 기본 생성자
+    Phone() {
+        System.out.println("기본 생성자 호출입니다.");
+        this.modelNumber = ++modelNumberCounter;
+        System.out.println(this.modelNumber + "번의 모델 번호를 발급했습니다.");
+    }
+
+    // 3.  사용자 정의 생성자
+    Phone(String modelName, String modelType, int modelCapacity, int modelPrice) {
+        this(); // 기본 생성자 호출
+        System.out.println("사용자 정의 생성자 호출입니다.");
+        this.modelName = modelName;
+        this.modelType = modelType;
+        this.modelCapacity = modelCapacity;
+        this.modelPrice = modelPrice;
+    }
+}
+*/
