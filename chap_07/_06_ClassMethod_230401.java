db6b47fe6c5bd82bc527e8a8085f39a27d0071bc
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2023-04-05 03:41:52 +0900

    Update _06_ClassMethod_230401.java

--- chap_07/_06_ClassMethod_230401.java
+++ chap_07/_06_ClassMethod_230401.java
@@ -2,20 +2,21 @@ package chap_07;
 
 public class _06_ClassMethod_230401 {
     public static void main(String[] args) {
-        // 1-1. 클래스 객체 생성
+        // 1. 객체 생성 전) 클래스 메서드 호출
+        Phone.useSafetyFeatures(); // 안전 기능을 시작합니다.
+        
+        // 2. 클래스 객체 생성
         Phone p = new Phone();
 
-        // 1-2. 클래스 메서드 호출(객체 생성 후 클래스 메서드 호출)
+        // 3. 객체 생성 후) 클래스 메서드 호출
         p.useSafetyFeatures(); // 안전 기능을 시작합니다.
-
-        // 2. 클래스 메서드 호출(객체 생성없이 클래스 메서드 호출)
-        Phone.useSafetyFeatures(); // 안전 기능을 시작합니다.
     }
 }
 
 /*
-// 클래스 메서드 정의
+// 클래스 메서드
 public class Phone {
+    // 클래스 메서드 정의
     static void useSafetyFeatures() {
         System.out.println("안전 기능을 시작합니다.");
         // modelName = "자바자바폰"; // 클래스 메서드 내에 인스턴스 변수 사용 불가
