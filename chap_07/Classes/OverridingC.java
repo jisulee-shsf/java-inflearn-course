567e3a5c697b38d8a8657bb5ec5495ff3bc36fe2
Jisu Lee <jisulee.shsf@gmail.com>
2023-04-05 00:45:09 +0900

    Update OverridingC.java

--- chap_07/Classes/OverridingC.java
+++ chap_07/Classes/OverridingC.java
@@ -1,16 +1,16 @@
 package chap_07.Classes;
 
-public class MethodOverridingC extends MethodOverridingA {
+public class OverridingC extends OverridingA {
 
     // 3-1. 생성자 정의
-    public MethodOverridingC() {
+    public OverridingC() {
         // 인스턴스 변수 초기화
         this.modelName = "자바폰3";
     }
 
     // 3-2. 메서드 정의(오버라이딩)
     public void feature1() {
-        System.out.println(this.modelName + ": MethodOverridingC에서 feature1의 오버라이딩입니다.");
+        System.out.println(this.modelName + ": OverridingC에서 feature1의 오버라이딩입니다.");
     }
 
     public void feature3() {
