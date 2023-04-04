6e4babb9b9a9e0500c3f54395bf2e99e6b1e9897
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2023-04-05 03:23:49 +0900

    Update _05_Overloading_230401.java

--- chap_07/_05_Overloading_230401.java
+++ chap_07/_05_Overloading_230401.java
@@ -7,9 +7,8 @@ public class _05_Overloading_230401 {
 
         // 2. 인스턴스 변수 값 설정
         p.modelName = "자바폰";
-        System.out.println(p.modelName); // 자바폰
 
-        // 3. 메서드 호출(오버로딩)
+        // 3-1. 오버로딩) 메서드1 호출
         p.safety(true, true);
         /*
         안전 기능이 시작합니다.
@@ -17,6 +16,7 @@ public class _05_Overloading_230401 {
         충돌 감지 기능을 시작합니다.
         */
 
+        // 3-2. 오버로딩) 메서드2 호출
         p.safety();
         /*
         안전 기능이 시작합니다.
@@ -29,7 +29,7 @@ public class _05_Overloading_230401 {
 /*
 // 오버로딩
 public class Phone {
-    // 1. safety() 메서드1
+    // 1. 메서드1
     void safety(boolean useEmergencySos, boolean useCrashDetection) {
         System.out.println("안전 기능을 시작합니다.");
         if (useEmergencySos) {
@@ -41,7 +41,7 @@ public class Phone {
         System.out.println();
     }
 
-    // 2. safety() 메서드2
+    // 2. 메서드2
     void safety() {
         safety(true, true); // 기본값 설정
     }
