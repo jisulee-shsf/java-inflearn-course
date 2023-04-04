b1baf5925c8be806b9afd6f5c1f27204044a7199
Jisu Lee <jisulee.shsf@gmail.com>
2023-04-05 00:37:33 +0900

    Update _13_Overriding_230402

--- chap_07/_13_Overriding_230402.java
+++ chap_07/_13_Overriding_230402.java
@@ -1,22 +1,22 @@
 package chap_07;
 
-import chap_07.Safety.*;
+import chap_07.Classes.*;
 
 public class _13_Overriding_230402 {
     public static void main(String[] args) {
         // 1. 클래스 객체 생성
-        MethodOverriding1 mo1 = new MethodOverriding1();
-        MethodOverriding2 mo2 = new MethodOverriding2();
-        MethodOverriding3 mo3 = new MethodOverriding3();
+        MethodOverridingA mo1 = new MethodOverridingA();
+        MethodOverridingB mo2 = new MethodOverridingB();
+        MethodOverridingC mo3 = new MethodOverridingC();
 
         // 2-1. 메서드 호출(오버라이딩 전)
         mo1.feature1();
         mo2.feature1();
         mo3.feature1();
         /*
-        안전1: feature1입니다.
-        안전2: feature1입니다.
-        안전3: feature1입니다.
+        자바폰1: feature1입니다.
+        자바폰2: feature1입니다.
+        자바폰3: feature1입니다.
         */
 
         // 2-2. 메서드 호출(오버라이딩 후)
@@ -24,9 +24,9 @@ public class _13_Overriding_230402 {
         mo2.feature1();
         mo3.feature1();
         /*
-        안전1: feature1입니다.
-        안전2: MethodOverriding2에서 feature1의 오버라이딩입니다.
-        안전3: MethodOverriding3에서 feature1의 오버라이딩입니다.
+        자바폰1: feature1입니다.
+        자바폰2: MethodOverridingB에서 feature1의 오버라이딩입니다.
+        자바폰3: MethodOverridingC에서 feature1의 오버라이딩입니다.
         */
     }
 }
