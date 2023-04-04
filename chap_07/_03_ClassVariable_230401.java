79a95771df65a7548c9537d35747b63894e329a5
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2023-04-05 02:30:52 +0900

    Update _03_ClassVariable_230401.java

--- chap_07/_03_ClassVariable_230401.java
+++ chap_07/_03_ClassVariable_230401.java
@@ -5,18 +5,14 @@ public class _03_ClassVariable_230401 {
         // 1. 클래스 객체 생성
         Phone p = new Phone();
 
-        // 2. 인스턴스 변수 값 설정
-        p.modelName = "자바폰";
-        System.out.println(p.modelName); // 자바폰
-
-        // 3. 클래스 변수 사용
+        // 2. 클래스 변수 출력
         System.out.println(p.canCrashDetection); // 객체 -> false
-        System.out.println(Phone.canCrashDetection); // 인스턴스 전체 -> false
+        System.out.println(Phone.canCrashDetection); // 전체 -> false
 
-        // 4. 클래스 변수 값 변경
+        // 3. 클래스 변수 값 변경 후 출력
         Phone.canCrashDetection = true;
-        System.out.println(p.canCrashDetection); // 객체 -> true
-        System.out.println(Phone.canCrashDetection); // 인스턴스 전체 -> true
+        System.out.println(p.canCrashDetection); //  객체 -> true
+        System.out.println(Phone.canCrashDetection); // 전체 -> true
     }
 }
 
