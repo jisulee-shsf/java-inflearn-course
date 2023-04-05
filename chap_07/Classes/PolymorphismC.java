694c842f527f5a580c665792c84d2d41efe6368b
Jisu Lee <jisulee.shsf@gmail.com>
2023-04-05 14:50:07 +0900

    Update PolymorphismC.java

---
+++ chap_07/Classes/PolymorphismC.java
@@ -0,0 +1,17 @@
+package chap_07.Classes;
+
+public class PolymorphismC extends PolymorphismA { // 조상 클래스
+    // 1-1. 인스턴스 변수 선언
+    public String modelName;
+
+    // 1-2. 생성자 정의
+    public PolymorphismC() {
+        // 인스턴스 변수 초기화
+        this.modelName = "자바폰3";
+    }
+
+    // 1-3. 메서드 정의
+    public void feature3() {
+        System.out.println(this.modelName + ": PolymorphismC 형변환을 통해 호출한 feature3입니다.");
+    }
+}
