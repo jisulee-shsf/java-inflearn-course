8c62700321ce3f3330fb749040428cbfcc2cbc21
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2023-04-05 10:58:53 +0900

    Update _10_AccessModifier_230402.java

--- chap_07/_10_AccessModifier_230402.java
+++ chap_07/_10_AccessModifier_230402.java
@@ -9,31 +9,29 @@ public class _10_AccessModifier_230402 {
         // 2. 인스턴스 변수 값 설정
         pu.modelPrice = 1000000;
         System.out.println(pu.modelPrice); // 1000000
-
         pu.modelPrice = -1000000; // 잘못된 값으로 설정될 수 있음
         System.out.println(pu.modelPrice); // -1000000
 
-        // 3. 접근 제어자 변경(public -> private) 후, 인스턴스 변수 값 재설정
-        // java: modelPrice has private access in chap_07.PhoneUpdated
+        // 3. public -> private) 접근 제어자 변경 및 메서드 호출
         pu.modelPrice = 1000000;
-        System.out.println(pu.modelPrice);
-
+        System.out.println(pu.modelPrice); // java: modelPrice has private access in chap_07.PhoneUpdated
         pu.modelPrice = -1000000;
-        System.out.println(pu.modelPrice);
+        System.out.println(pu.modelPrice); // java: modelPrice has private access in chap_07.PhoneUpdated
         */
 
-        // 4. 메서드 호출(getter & setter)
+        // 4. getter & setter) 메서드 호출
         pu.setModelPrice(1500000);
         System.out.println(pu.getModelPrice()); // 1500000
     }
 }
 
 /*
+// 접근 제어자
 public class PhoneUpdated {
-    // 1. 인스턴스 변수 선언
-    private int modelPrice; // 접근 제어자 변경(public -> private)
+    // 1. private) 인스턴스 변수 선언
+    private int modelPrice; // 모델 가격
 
-    // 2. 메서드 정의(getter & setter)
+    // 2. getter & setter 메서드 정의
     public int getModelPrice() {
         return modelPrice;
     }
