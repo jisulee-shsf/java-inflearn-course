3bf292b03a84d08e10bf5ed84a061dfacc97ac34
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2023-04-06 04:45:55 +0900

    Update InheritanceC.java

--- chap_07/Classes/InheritanceC.java
+++ chap_07/Classes/InheritanceC.java
@@ -3,7 +3,6 @@ package chap_07.Classes;
 public class InheritanceC extends InheritanceA { // 자손 클래스
     // 3-1. 생성자 정의
     public InheritanceC() {
-        // 인스턴스 변수 초기화
         this.modelName = "자바폰3";
     }
 
