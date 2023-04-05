86af644b202c6ecb86aa4b92f3aa3d0e57f66c9c
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2023-04-05 13:48:29 +0900

    Update _12_Inheritance_230402.java

--- chap_07/_12_Inheritance_230402.java
+++ chap_07/_12_Inheritance_230402.java
@@ -4,39 +4,27 @@ import chap_07.Classes.*;
 
 public class _12_Inheritance_230402 {
     public static void main(String[] args) {
-        // 1. 클래스 객체 생성 및 호출
+        // 1. 클래스 객체 생성 및 출력
         InheritanceA i1 = new InheritanceA();
         InheritanceB i2 = new InheritanceB();
         InheritanceC i3 = new InheritanceC();
-
+        
         System.out.println(i1.modelName); // 자바폰1
         System.out.println(i2.modelName); // 자바폰2
         System.out.println(i3.modelName); // 자바폰3
 
-        // 2. 메서드 호출(상속 전)
-        // Feature1 -> System.out.println("featureA")
+       
+        // 2-1. 상속) 메서드 호출
         i1.feature1();
-        i2.feature2();
-        i3.feature3();
+        i2.feature1();
+        i3.feature1();
         /*
         자바폰1: feature1입니다.
-        자바폰2: feature2입니다.
-        자바폰3: feature3입니다.
+        자바폰2: feature1입니다.
+        자바폰3: feature1입니다.
         */
 
-        System.out.println();
-
-        // 3-1. 메서드 호출(상속 후)
-        i1.feature1();
-        i2.feature2();
-        i3.feature3();
-        /*
-        자바폰1: feature1입니다.
-        자바폰2: feature2입니다.
-        자바폰3: feature3입니다.
-        */
-
-        // 3-2. 메서드 호출(자손 클래스)
+        // 2-2. 자손 클래스) 메서드 호출
         i2.feature2();
         i3.feature3();
         /*
