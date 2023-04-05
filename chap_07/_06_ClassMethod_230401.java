5fa62a3d4e9bd260ecc66723a8360f6cc7a39e76
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2023-04-05 09:15:32 +0900

    Update _06_ClassMethod_230401.java

--- chap_07/_06_ClassMethod_230401.java
+++ chap_07/_06_ClassMethod_230401.java
@@ -19,8 +19,8 @@ public class Phone {
     // 클래스 메서드 정의
     static void useSafetyFeatures() {
         System.out.println("안전 기능을 시작합니다.");
-        // modelName = "자바자바폰"; // 클래스 메서드 내에 인스턴스 변수 사용 불가
-        // canCrashDetection = false; // 클래스 메서드 내에 클래스 변수 사용 가능
+        // modelName = "자바자바폰"; 인스턴스 변수 사용 불가 확인
+        // canCrashDetection = false; 클래스 변수 사용 가능 확인
     }
 }
 */
