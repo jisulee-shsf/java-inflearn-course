2910253c92be42d918a18913716a1beb02c9ac6e
Jisu Lee <jisulee.shsf@gmail.com>
2023-04-05 20:40:39 +0900

    Upload file

---
+++ chap_07/Classes/SuperA.java
@@ -0,0 +1,21 @@
+package chap_07.Classes;
+
+public class SuperA { // 조상 클래스
+    // 1-1. 인스턴스 변수 선언
+    public String modelName;
+
+    // 1-2. 생성자 정의
+    public SuperA() {
+        this("super자바폰1");
+    }
+
+    protected SuperA(String modelName) {
+        // 인스턴스 변수 초기화
+        this.modelName = modelName;
+    }
+
+    // 1-3. 메서드 정의
+    public void feature1() {
+        System.out.println(this.modelName + ": 참조 변수 super를 사용한 feature1입니다.");
+    }
+}
---
+++ chap_07/Classes/SuperC.java
@@ -0,0 +1,14 @@
+package chap_07.Classes;
+
+public class SuperC extends SuperA { // 자식 클래스
+    // 3-1. 생성자 호출
+    public SuperC() {
+        super("super자바폰3");
+    }
+
+    // 3-2. 메서드 정의
+    public void feature3() {
+        super.feature1(); // 조상 클래스 메서드 호출
+        System.out.println(this.modelName + ": feature3입니다.");
+    }
+}
