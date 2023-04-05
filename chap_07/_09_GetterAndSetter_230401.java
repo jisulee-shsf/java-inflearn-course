c7153718fa561f77aebdb56028dbee43e627b93c
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2023-04-05 10:42:46 +0900

    Update _09_GetterAndSetter_230401.java

--- chap_07/_09_GetterAndSetter_230401.java
+++ chap_07/_09_GetterAndSetter_230401.java
@@ -5,13 +5,13 @@ public class _09_GetterAndSetter_230401 {
         // 1. 클래스 객체 생성
         Phone p = new Phone();
 
-        // 2-1. 메서드 호출(setter)
+        // 2-1. setter) 메서드 호출
         p.setModelName("자바폰");
         // p.setModelType("프로맥스"); 인스턴스 변수 값 누락
         p.setModelCapacity(120); // -> 용량 정보 오기입
         p.setModelPrice(1000000);
 
-        // 2-2. 메서드 호출(getter)
+        // 2-2. getter) 메서드 호출
         System.out.println("모델명: " + p.getModelName());
         System.out.println("모델 타입: " + p.getModelType());
         System.out.println("모델 용량: " + p.getModelCapacity());
@@ -28,19 +28,26 @@ public class _09_GetterAndSetter_230401 {
 /*
 // getter & setter
 public class Phone {
-    // 1. modelType 인스턴스 변수의 getter & setter 메서드 정의
+    // 1. modelName getter & setter 메서드 정의
+    String getModelName() {
+        return modelName;
+    }
+    void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    // 2. modelType getter & setter 메서드 정의
     String getModelType() {
         if (modelType == null || modelType.isEmpty()) {
             return "오류가 발생했습니다.";
         }
         return modelType;
     }
-
     void setModelType(String modelType) {
         this.modelType = modelType;
     }
-
-    // 2. modelCapacity 인스턴스 변수의 getter & setter 메서드 정의
+    
+    // 3. modelCapacity getter & setter 메서드 정의
     int getModelCapacity() {
         return modelCapacity;
     }
@@ -51,6 +58,14 @@ public class Phone {
         } else {
             this.modelCapacity = modelCapacity;
         }
+    }
+    
+    // 4. modelPrice getter & setter 메서드 정의
+    int getModelPrice() {
+        return modelPrice;
+    }
+    void setModelPrice(int modelPrice) {
+        this.modelPrice = modelPrice;
     }
 }
 */
