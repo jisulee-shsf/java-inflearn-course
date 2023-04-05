8deb3c5d3b4253cd9692fda3694f84acb2df14d2
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2023-04-05 09:55:51 +0900

    Update _08_Constructor_230401.java

--- chap_07/_08_Constructor_230401.java
+++ chap_07/_08_Constructor_230401.java
@@ -9,7 +9,7 @@ public class _08_Constructor_230401 {
         1번의 모델 번호를 발급했습니다.
         */
 
-        // 2. 사용자 정의 생성자 호출(this())
+        // 2. this()) 기본 ・ 사용자 정의 생성자 호출
         Phone p2 = new Phone("자바폰", "프로", 128, 1000000);
         System.out.println(p2.modelName);
         System.out.println(p2.modelType);
@@ -32,20 +32,21 @@ public class _08_Constructor_230401 {
 /*
 // 생성자
 public class Phone {
-    // 1. 클래식 변수 정의
-    static int modelNumberCounter = 0;
+    // 1. 인스턴스 및 클래스 변수 선언
+    int modelNumber; // 모델 번호
+    static int modelNumberCounter = 0; // modelNumber 증가
 
     // 2. 기본 생성자
     Phone() {
-        System.out.println("기본 생성자 호출입니다.");
+        System.out.println("기본 생성자 호출입니다. ");
         this.modelNumber = ++modelNumberCounter;
         System.out.println(this.modelNumber + "번의 모델 번호를 발급했습니다.");
     }
 
-    // 3.  사용자 정의 생성자
+    // 3. 사용자 정의 생성자
     Phone(String modelName, String modelType, int modelCapacity, int modelPrice) {
         this(); // 기본 생성자 호출
-        System.out.println("사용자 정의 생성자 호출입니다.");
+        System.out.println("사용자 정의 생성자 호출입니다."); // 인스턴스 변수의 초기화 코드
         this.modelName = modelName;
         this.modelType = modelType;
         this.modelCapacity = modelCapacity;
