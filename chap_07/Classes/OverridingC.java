0e588c36126a4a4443ae1eda3c86f930fbc922bb
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2023-04-05 13:59:57 +0900

    Update OverridingC.java

--- chap_07/Classes/OverridingC.java
+++ chap_07/Classes/OverridingC.java
@@ -8,7 +8,7 @@ public class OverridingC extends OverridingA {
         this.modelName = "자바폰3";
     }
 
-    // 3-2. 메서드 정의(오버라이딩)
+    // 3-2. 오버라이딩) 메서드 정의
     public void feature1() {
         System.out.println(this.modelName + ": OverridingC에서 feature1의 오버라이딩입니다.");
     }
