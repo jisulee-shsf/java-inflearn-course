285661383e312805b55b62d09ac1e47ededce72a
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2023-04-09 01:26:52 +0900

    Update AbstractClassB.java

--- chap_08/classes/AbstractClassB.java
+++ chap_08/classes/AbstractClassB.java
@@ -2,7 +2,7 @@ package chap_08.classes;
 
 public class AbstractClassB extends AbstractClassA {
 
-    // 추상 메서드 -> 메서드 정의
+    // 추상 메서드 -> 메서드 
     @Override
     public void findSth() {
         System.out.println("AbstractClassB에서 findSth을 시작합니다.");
