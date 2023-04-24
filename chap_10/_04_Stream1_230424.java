f41096c46c771c870ca59da9b74bea3160bce91f
Jisu Lee <jisulee.shsf@gmail.com>
2023-04-24 20:31:28 +0900

    Upload file

---
+++ chap_10/_04_Stream1_230424.java
@@ -0,0 +1,29 @@
+package chap_10;
+
+import java.util.List;
+import java.util.Arrays;
+import java.util.stream.Stream;
+import java.util.stream.IntStream;
+
+import static java.util.Arrays.stream;
+
+public class _04_Stream1_230424 {
+    public static void main(String[] args) {
+        // 1. Collection.stream()
+        List<Integer> intList = Arrays.asList(1, 2, 3, 4, 5);
+        Stream<Integer> intListStream = intList.stream();
+
+        List<String> stringList = Arrays.asList("A", "B", "C", "D", "E");
+        Stream<String> stringListStream = stringList.stream();
+
+        // 2. Arrays.stream()
+        int[] intArray = {1, 2, 3, 4, 5};
+        IntStream intArrayStream = Arrays.stream(intArray);
+
+        String[] stringArray = {"A", "B", "C", "D", "E"};
+        Stream<String> stringArrayStream = Arrays.stream(stringArray);
+
+        // 3. Stream.of()
+        Stream<String> streamOfStream = Stream.of("A", "B", "C", "D", "E");
+    }
+}
