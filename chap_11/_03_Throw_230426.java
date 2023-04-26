69856aa0b0a7fbdf16dbe9b827e03f147a56eff9
Jisu Lee <jisulee.shsf@gmail.com>
2023-04-26 20:36:17 +0900

    Upload file

---
+++ chap_11/_03_Throw_230426.java
@@ -0,0 +1,22 @@
+package chap_11;
+
+public class _03_Throw_230426 {
+    public static void main(String[] args) {
+        try {
+            int code = 1111;
+            if (code != 1234) {
+                throw new Exception("코드 입력에 오류가 발생했습니다.");
+            } else {
+                System.out.println("프로그램을 실행합니다.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        };
+        System.out.println("프로그램 종료");
+        /*
+        java.lang.Exception: 코드 입력에 오류가 발생했습니다.
+	    at chap_11._03_Throw_230426.main(_03_Throw_230426.java:8)
+        프로그램 종료
+        */
+    }
+}
---
+++ chap_11/_04_Finally_230426.java
@@ -0,0 +1,46 @@
+package chap_11;
+
+public class _04_Finally_230426 {
+    public static void main(String[] args) {
+        // 1. finally 블럭(정상 실행)
+        try {
+            System.out.println(100 / 2);
+            System.out.println("계산 정상 실행");
+        } catch (Exception e) {
+            System.out.println("오류 확인: " + e.getMessage());
+        } finally {
+            System.out.println("프로그램 종료");
+        }
+        /*
+        50
+        계산 정상 실행
+        프로그램 종료
+        */
+
+        // 2. finally 블럭(오류 발생)
+        try {
+            System.out.println(100 / 0);
+            throw new Exception("계산 오류 발생");
+        } catch (Exception e) {
+            System.out.println("오류 확인: " + e.getMessage());
+        } finally {
+            System.out.println("프로그램 종료");
+        }
+        /*
+        오류 확인: / by zero
+        프로그램 종료
+        */
+
+        // 3. try-finally(예외 처리 생략)
+        try {
+            System.out.println(100 / 0);
+        } finally {
+            System.out.println("프로그램 종료");
+        }
+        /*
+        프로그램 종료
+        Exception in thread "main" java.lang.ArithmeticException: / by zero
+	    at chap_11._04_Finally_230426.main(_04_Finally_230426.java:37)
+        */
+    }
+}
