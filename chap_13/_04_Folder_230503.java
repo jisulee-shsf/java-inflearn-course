794acd3b5dacb7d157832b1f2b7d85d16581dab7
Jisu Lee <jisulee.shsf@gmail.com>
2023-05-03 15:23:29 +0900

    Upload file

---
+++ chap_13/_04_Folder_230503.java
@@ -0,0 +1,33 @@
+package chap_13;
+
+import java.io.File;
+
+public class _04_Folder_230503 {
+    public static void main(String[] args) {
+        File folder1 = new File("AAA");
+        folder1.mkdir();
+        if (folder1.exists()) {
+            System.out.println("폴더 이름: " + folder1.getName());
+            System.out.println("폴더 경로: " + folder1.getAbsoluteFile());
+        } else {
+            System.out.println("폴더 없음");
+        }
+        /*
+        폴더 이름: AAA
+        폴더 경로: /Users/jisulee/workspace/java/AAA
+        */
+
+        File folder2 = new File("AAA/BBB/CCC");
+        folder1.mkdirs();
+        if (folder1.exists()) {
+            System.out.println("폴더 이름: " + folder2.getName());
+            System.out.println("폴더 경로: " + folder2.getAbsoluteFile());
+        } else {
+            System.out.println("폴더 없음");
+        }
+        /*
+        폴더 이름: CCC
+        폴더 경로: /Users/jisulee/workspace/java/AAA/BBB/CCC
+        */
+    }
+}
