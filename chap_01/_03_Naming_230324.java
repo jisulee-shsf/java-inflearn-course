a057f2259a117dd05516c070de67fa4b465bfca1
Jisu Lee <jisulee.shsf@gmail.com>
2023-05-08 00:32:39 +0900

    Update _03_Naming_230324.java

--- chap_01/_03_Naming_230324.java
+++ chap_01/_03_Naming_230324.java
@@ -3,14 +3,13 @@ package chap_01;
 public class _03_Naming_230324 {
     public static void main(String[] args) {
         /*
-        1. 변수
+        1. 변수명 짓기
             1) 저장 값에 어울리는 이름 사용
-            2) 한 개 또는 두 개 이상의 단어를 연속해 사용
-            3) 소문자로 시작하는 첫 단어 외 단어의 시작은 대문자 사용
+            2) 한 개 또는 두 개 이상의 단어를 연속 사용
+            3) 공백 외 밑줄(_), 문자(abc), 숫자(123) 사용
             4) 숫자 외 밑줄(_) 및 문자(abc)로 시작
-            5) 공백 외 밑줄(_), 문자(abc), 숫자(123) 사용
-            6) 예약어(public, static, ...) 사용 불가
-            7) 프로그램 흐름을 위한 변수명은 중요하지 않음
+            5) 첫 단어 외 대문자로 시작
+            6) 예약어 사용 불가
         */
         String nationality = "대한민국";
         String firstName = "길동";
@@ -21,20 +20,18 @@ public class _03_Naming_230324 {
         String flightNo = "KE123";
         String _flightNo = "KE123";
         String flight_No = "KE123";
-        int i = 10;
-        String s = "10";
-        String str = "10";
+        int accompany = 10;
+        int lengthOfStay = 10;
 
-        /* 2. 상수
+        /*
+        2. 상수
             1) 예약어 final 및 대문자 사용
             2) 두 개 이상의 단어는 밑줄(_)로 구분
-         */
+        */
         final String CODE = "KR"; // CODE = "US" 변경 불가
+        final double PI = 3.141592;
+
         final String KR_COUNTRY_CODE = "+82";
-        System.out.println(KR_COUNTRY_CODE);
-        
-        final double PI = 3.121592;
         final String DATE_OF_BIRTH = "2000-01-01";
-        System.out.println(DATE_OF_BIRTH);
     }
 }
