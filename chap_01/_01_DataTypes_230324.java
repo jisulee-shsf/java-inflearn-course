6ab9ba4fe79678aab963d40541884127a18d62e3
Jisu Lee <jisulee.shsf@gmail.com>
2023-05-07 23:34:44 +0900

    Update _01_DataTypes_230324.java

--- chap_01/_01_DataTypes_230324.java
+++ chap_01/_01_DataTypes_230324.java
@@ -2,17 +2,30 @@ package chap_01;
 
 public class _01_DataTypes_230324 {
     public static void main(String[] args) {
-        // 1. 문자열
+        // 1. 문자
         System.out.println("hello world");
         System.out.println("안녕하세요");
+        /*
+        hello world
+        안녕하세요
+        */
 
         // 2. 숫자
         System.out.println(123);
         System.out.println(-123);
         System.out.println(3.14);
+        /*
+        123
+        -123
+        3.14
+        */
 
-        // 3. 불
+        // 3. 불리안
         System.out.println(true);
         System.out.println(false);
+        /*
+        true
+        false
+        */
     }
 }
