a7e89c21496d8fe97de62ddcaf97cfd6057df65f
Jisu Lee <jisulee.shsf@gmail.com>
2023-05-09 00:13:01 +0900

    Update _03_EscapeSequence_230327.java

--- chap_03/_03_EscapeSequence_230327.java
+++ chap_03/_03_EscapeSequence_230327.java
@@ -9,20 +9,41 @@ public class _03_EscapeSequence_230327 {
         System.out.println(s1);
         System.out.println(s2);
         System.out.println(s3);
-        System.out.println("I\nlike\nJav.");
+        System.out.println("I\nlike\nJava.");
+        /*
+        I
+        like
+        Java.
+        I
+        like
+        Java.
+        */
 
         // 2. \t: 탭
         System.out.println("score\t95.5");
         System.out.println("grade\tA");
+        /*
+        score	95.5
+        grade	A
+        */
 
         // 3. \\: 역슬래시
-        System.out.println("C:\\Program Files\\Java"); // C:\Program Files\Java
+        System.out.println("C:\\Program Files\\Java");
+        /*
+        C:\Program Files\Java
+        */
 
-        // 4. \" & \': 큰따옴표 / 작은따옴표
-        System.out.println("그녀는 \"자바는 재밌어\"라고 말했습니다."); // 그녀는 "자바는 재밌어"라고 말했습니다.
-        System.out.println("그녀가 가장 좋아하는 것은 \'자바\'이다."); // 그녀가 가장 좋아하는 것은 '자바'이다.
-
+        // 4. \": 큰따옴표 & \': 작은따옴표
+        System.out.println("그녀는 \"자바는 재밌어\"라고 말했습니다.");
+        System.out.println("그녀가 가장 좋아하는 것은 \'자바\'입니다.");
+        System.out.println("그녀가 가장 좋아하는 것은 '자바'입니다.");
         char c = '\'';
-        System.out.println(c); // '
+        System.out.println(c);
+        /*
+        그녀는 "자바는 재밌어"라고 말했습니다.
+        그녀가 가장 좋아하는 것은 '자바'입니다.
+        그녀가 가장 좋아하는 것은 '자바'입니다.
+        '
+        */
     }
 }
