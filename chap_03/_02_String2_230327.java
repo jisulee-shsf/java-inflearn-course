066b14e4eeb37f4c991dc0ef331ee94912dcedd7
Jisu Lee <jisulee.shsf@gmail.com>
2023-05-09 00:12:48 +0900

    Update _02_String2_230327.java

--- chap_03/_02_String2_230327.java
+++ chap_03/_02_String2_230327.java
@@ -2,18 +2,16 @@ package chap_03;
 
 public class _02_String2_230327 {
     public static void main(String[] args) {
-        // 1. equals() & equalsIgnoreCase(): 문자열 내용이 동일한지 비교 / 문자열 내용이 대소문자 관계없이 동일한지 비교
         String s1 = "Java";
         String s2 = "Python";
-        System.out.println(s1);
-        System.out.println(s2);
 
+        // 1. equals(): 문자열 내용이 동일한지 비교 / equalsIgnoreCase(): 문자열 내용이 대소문자 관계없이 동일한지 비교
         System.out.println(s1.equals(s2)); // false
         System.out.println(s1.equals("Java")); // true
         System.out.println(s1.equals("java")); // false
         System.out.println(s1.equalsIgnoreCase("java")); // true
 
-        // 2. equals() 내용이 동일한지 비교 / == 참조가 동일한지 비교
+        // 2. equals(): 내용이 동일한지 비교 / ==: 참조가 동일한지 비교
         String s3 = "Java";
         String s4 = "Java";
         System.out.println(s3.equals(s4)); // true
