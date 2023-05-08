6e1a84e87a8e29714c351417bf7be91462dee618
Jisu Lee <jisulee.shsf@gmail.com>
2023-05-08 21:57:33 +0900

    Update _04_Operator4_230326.java

--- chap_02/_04_Operator4_230326.java
+++ chap_02/_04_Operator4_230326.java
@@ -1,25 +1,25 @@
 package chap_02;
 
-public class _04_LogicalOperator_230326 {
+public class _04_Operator4_230326 {
     public static void main(String[] args) {
         // 1. 논리 연산자
         boolean a = true;
         boolean b = true;
         boolean c = true;
-        System.out.println(a || b || c); // true
         System.out.println(a && b && c); // true
+        System.out.println(a || b || c); // true
 
         c = false;
-        System.out.println(a || b || c); // true
         System.out.println(a && b && c); // false
-
-        System.out.println((5 > 3) || (3 > 1)); // true
-        System.out.println((5 > 3) || (3 < 1)); // true
-        System.out.println((5 < 3) || (3 < 1)); // false
+        System.out.println(a || b || c); // true
 
         System.out.println((5 > 3) && (3 > 1)); // true
         System.out.println((5 > 3) && (3 < 1)); // false
         System.out.println((5 < 3) && (3 < 1)); // false
+
+        System.out.println((5 > 3) || (3 > 1)); // true
+        System.out.println((5 > 3) || (3 < 1)); // true
+        System.out.println((5 < 3) || (3 < 1)); // false
 
         // 2. 논리 부정 연산자
         System.out.println(!a); // false
