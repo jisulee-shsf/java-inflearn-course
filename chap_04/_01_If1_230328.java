bd8f13565f584fbc6916deb25ba63995eb190ecf
Jisu Lee <jisulee.shsf@gmail.com>
2023-05-10 00:17:26 +0900

    Update _01_If1_230328.java

--- chap_04/_01_If1_230328.java
+++ chap_04/_01_If1_230328.java
@@ -2,51 +2,38 @@ package chap_04;
 
 public class _01_If1_230328 {
     public static void main(String[] args) {
-        // 1-1. if문의 조건이 참인 경우 & {}를 생략한 경우
+        // 1. if문 내에 수행 문장이 1개인 경우 & {}를 생략한 경우
         int hour = 6;
         if (hour == 6)
             System.out.println("PT를 시작합니다.");
-            System.out.println("PT를 진행 중입니다.");
-        System.out.println("PT를 종료합니다. #1-1");
+        System.out.println("PT를 종료합니다. #1");
         /*
         PT를 시작합니다.
-        PT를 진행 중입니다.
-        PT를 종료합니다. #1-1
+        PT를 종료합니다. #1
         */
 
-        // 1-2. if문의 조건이 참인 경우 & {}를 표기한 경우
+        // 2. if문 내에 수행 문장이 2개인 경우 & {}를 생략한 경우
+        hour = 8;
+        if (hour == 6)
+            System.out.println("PT를 시작합니다.");
+            System.out.println("PT를 진행 중입니다.");
+        System.out.println("PT를 종료합니다. #2");
+        /*
+        PT를 진행 중입니다.
+        PT를 종료합니다. #2
+        */
+
+        // 3. if문 내에 수행 문장이 2개인 경우 & {}를 표기한 경우
         hour = 6;
         if (hour == 6) {
             System.out.println("PT를 시작합니다.");
             System.out.println("PT를 진행 중입니다.");
         }
-        System.out.println("PT를 종료합니다. #1-2");
+        System.out.println("PT를 종료합니다. #3");
         /*
         PT를 시작합니다.
         PT를 진행 중입니다.
-        PT를 종료합니다. #1-2
-        */
-
-        // 2-1. if문의 조건이 거짓인 경우 & {}를 생략한 경우
-        hour = 7;
-        if (hour == 6)
-            System.out.println("PT를 시작합니다.");
-            System.out.println("PT를 진행 중입니다.");
-        System.out.println("PT를 종료합니다. #2-1");
-        /*
-        PT를 진행 중입니다.
-        PT를 종료합니다. #2-1
-        */
-
-        // 2-2. if문의 조건이 거짓인 경우 & {}를 표기한 경우
-        hour = 7;
-        if (hour == 6) {
-            System.out.println("PT를 시작합니다.");
-            System.out.println("PT를 진행 중입니다.");
-        }
-        System.out.println("PT를 종료합니다. #2-2");
-        /*
-        PT를 종료합니다. #2-2
+        PT를 종료합니다. #3
         */
     }
 }
