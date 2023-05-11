5c901931f26a8f99464cbb001ca569ad6a441ec0
Jisu Lee <jisulee.shsf@gmail.com>
2023-05-11 16:42:38 +0900

    Update _04_ElseIf_230328.java

--- chap_04/_04_ElseIf_230328.java
+++ chap_04/_04_ElseIf_230328.java
@@ -2,24 +2,49 @@ package chap_04;
 
 public class _04_ElseIf_230328 {
     public static void main(String[] args) {
-        // if-else if문
-        boolean latte = false;
-        boolean mocha = true;
-        if (latte) { // false -> else if문 이동
-            System.out.println("latte를 주문하셨습니다.");
+        // 1. if-else if
+        boolean caffeLatte = false;
+        boolean caffeMocha = false;
+        boolean matchaFrappuccino = true;
+
+        if (caffeLatte) { // false -> else if 이동
+            System.out.println("카페 라떼를 주문하셨습니다.");
             System.out.println("음료를 제조하고 있습니다. #1");
-        } else if (mocha) { // true -> 수행
-            System.out.println("mocha를 주문하셨습니다.");
+        } else if (caffeMocha) { // false -> else if 이동
+            System.out.println("카페 모카를 주문하셨습니다.");
             System.out.println("음료를 제조하고 있습니다. #2");
+        } else if (matchaFrappuccino) { // true -> 수행
+            System.out.println("말차 프라푸치노를 주문하셨습니다.");
+            System.out.println("음료를 제조하고 있습니다. #3");
         } else {
-            System.out.println("latte와 mocha가 모두 소진되어 americano를 주문하셨습니다.");
+            System.out.println("아메리카노를 주문하셨습니다.");
+            System.out.println("음료를 제조하고 있습니다. #4");
+        }
+        System.out.println("주문이 완료됐습니다.");
+        /*
+        말차 프라푸치노를 주문하셨습니다.
+        음료를 제조하고 있습니다. #3
+        주문이 완료됐습니다.
+        */
+
+        // 2. else가 생략된 경우
+        caffeLatte = false;
+        caffeMocha = false;
+        matchaFrappuccino = false;
+
+        if (caffeLatte) { // false -> else if 이동
+            System.out.println("카페 라떼를 주문하셨습니다.");
+            System.out.println("음료를 제조하고 있습니다. #1");
+        } else if (caffeMocha) { // false -> else if 이동
+            System.out.println("카페 모카를 주문하셨습니다.");
+            System.out.println("음료를 제조하고 있습니다. #2");
+        } else if (matchaFrappuccino) { // false -> 종료
+            System.out.println("말차 프라푸치노를 주문하셨습니다.");
             System.out.println("음료를 제조하고 있습니다. #3");
         }
-        System.out.println("주문하신 음료 나왔습니다.");
+        System.out.println("주문이 완료됐습니다.");
         /*
-        mocha를 주문하셨습니다.
-        음료를 제조하고 있습니다. #2
-        주문하신 음료 나왔습니다.
+        주문이 완료됐습니다.
         */
     }
 }
