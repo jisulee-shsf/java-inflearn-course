8f93b3d85bb86b837be5d08a9b5bdaf2d7438f68
Jisu Lee <jisulee.shsf@gmail.com>
2023-05-11 16:17:35 +0900

    Update _01_If1_230328.java

--- chap_04/_01_If1_230328.java
+++ chap_04/_01_If1_230328.java
@@ -4,7 +4,7 @@ public class _01_If1_230328 {
     public static void main(String[] args) {
         // 1. if문 내에 수행 문장이 1개인 경우 & {}를 생략한 경우
         int hour = 6;
-        if (hour == 6)
+        if (hour <= 6)
             System.out.println("PT를 시작합니다.");
         System.out.println("PT를 종료합니다. #1");
         /*
@@ -13,8 +13,8 @@ public class _01_If1_230328 {
         */
 
         // 2. if문 내에 수행 문장이 2개인 경우 & {}를 생략한 경우
-        hour = 8;
-        if (hour == 6)
+        hour = 10;
+        if (hour <= 6)
             System.out.println("PT를 시작합니다.");
             System.out.println("PT를 진행 중입니다.");
         System.out.println("PT를 종료합니다. #2");
@@ -25,7 +25,7 @@ public class _01_If1_230328 {
 
         // 3. if문 내에 수행 문장이 2개인 경우 & {}를 표기한 경우
         hour = 6;
-        if (hour == 6) {
+        if (hour <= 6) {
             System.out.println("PT를 시작합니다.");
             System.out.println("PT를 진행 중입니다.");
         }
