b2ccfce9f1d82f1dc7b0bd87d1229e38ff8577dc
Jisu Lee <jisulee.shsf@gmail.com>
2023-05-11 21:10:53 +0900

    Update _06_For_230328.java

--- chap_04/_06_For_230328.java
+++ chap_04/_06_For_230328.java
@@ -4,36 +4,43 @@ public class _06_For_230328 {
     public static void main(String[] args) {
         // 1. for문
         for (int i = 0; i < 10; i++) {
-            System.out.print(i); // 0123456789
+            System.out.print(i + " "); // 0 1 2 3 4 5 6 7 8 9
+
         }
 
-        System.out.println();
-
-        // 2-1. 짝수 ・ 홀수 출력
+        // 2-1. 짝수 & 홀수를 구하는 경우
         for (int i = 0; i < 10; i += 2) {
-            System.out.print(i); // 02468
+            System.out.print(i + " "); // 0 2 4 6 8
         }
 
         for (int i = 1; i < 10; i += 2) {
-            System.out.print(i); //13579
+            System.out.print(i + " "); // 1 3 5 7 9
         }
 
         System.out.println();
-
-        // 2-2. 숫자 거꾸로 출력
+        // 2-2. 숫자 거꾸로 출력하는 경우
         for (int i = 10; i > 0; i--) {
-            System.out.print(i); //10987654321
+            System.out.print(i + " "); // 10 9 8 7 6 5 4 3 2 1
         }
 
-        System.out.println();
-
-        // 2-3. 숫자 총합 출력
+        // 2-3. 숫자 총합을 구하는 경우
         int sum = 0;
         for (int i = 1; i < 11; i++) {
-            System.out.print(i); // 12345678910
             sum += i;
+            System.out.println(i + ":\t" + sum);
+            /*
+            1:	1
+            2:	3
+            3:	6
+            4:	10
+            5:	15
+            6:	21
+            7:	28
+            8:	36
+            9:	45
+            10:	55
+            */
         }
-        System.out.println();
-        System.out.print(sum); // 55
+        System.out.println(sum); // 55
     }
 }
