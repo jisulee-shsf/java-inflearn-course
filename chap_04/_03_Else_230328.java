eb74667103491179f23830bb3d3b69f31c49779b
Jisu Lee <jisulee.shsf@gmail.com>
2023-05-11 19:11:38 +0900

    Update _03_Else_230328.java

--- chap_04/_03_Else_230328.java
+++ chap_04/_03_Else_230328.java
@@ -2,7 +2,7 @@ package chap_04;
 
 public class _03_Else_230328 {
     public static void main(String[] args) {
-        // 1. if문의 조건식이 참인 경우
+        // if-else문
         int hour = 6;
         if (hour <= 6) {
             System.out.println("PT를 시작합니다."); // 6시까지 도착하면, PT 시작
@@ -15,7 +15,6 @@ public class _03_Else_230328 {
         PT를 종료합니다. #1
         */
 
-        // 2. if문의 조건식이 거짓인 경우 -> else블럭 수행
         hour = 10;
         boolean breakfast = false;
         if (hour <= 6 && !breakfast) {
