2d7cd774d55c31356d0012a03ea3d4b9f74ea78a
Jisu Lee <jisulee.shsf@gmail.com>
2023-05-11 20:00:39 +0900

    Update _05_Switch_230328.java

--- chap_04/_05_Switch_230328.java
+++ chap_04/_05_Switch_230328.java
@@ -39,5 +39,28 @@ public class _05_Switch_230328 {
         2위 입상자는 100만원의 장학금을 수여받습니다.
         장학금 안내를 종료합니다. #2
         */
+
+        // 3. break 여부에 따른 비용 변화 확인
+        ranking = 3;
+        scholarship = 100;
+
+        switch (ranking) {
+            case 1:
+                scholarship += 100;
+            case 2:
+            case 3:
+                scholarship += 100;
+                System.out.println(ranking + ": " + scholarship + "만원");
+        }
+        System.out.println("장학금 계산을 종료합니다. #3");
+        /*
+        ranking 1)
+        300만원
+        장학금 계산을 종료합니다. #3
+
+        ranking 2 & 3)
+        200만원
+        장학금 계산을 종료합니다. #3
+        */
     }
 }
