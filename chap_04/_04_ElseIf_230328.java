93dec308bfd8e5ba4bcf7be1ba2847faf84a61f7
Jisu Lee <jisulee.shsf@gmail.com>
2023-05-11 19:11:47 +0900

    Update _04_ElseIf_230328.java

--- chap_04/_04_ElseIf_230328.java
+++ chap_04/_04_ElseIf_230328.java
@@ -2,18 +2,18 @@ package chap_04;
 
 public class _04_ElseIf_230328 {
     public static void main(String[] args) {
-        // 1. if-else if
+        // 1. if-else if문
         boolean caffeLatte = false;
         boolean caffeMocha = false;
         boolean matchaFrappuccino = true;
 
-        if (caffeLatte) { // false -> else if 이동
+        if (caffeLatte) { // false
             System.out.println("카페 라떼를 주문하셨습니다.");
             System.out.println("음료를 제조하고 있습니다. #1");
-        } else if (caffeMocha) { // false -> else if 이동
+        } else if (caffeMocha) { // false
             System.out.println("카페 모카를 주문하셨습니다.");
             System.out.println("음료를 제조하고 있습니다. #2");
-        } else if (matchaFrappuccino) { // true -> 수행
+        } else if (matchaFrappuccino) { // true
             System.out.println("말차 프라푸치노를 주문하셨습니다.");
             System.out.println("음료를 제조하고 있습니다. #3");
         } else {
@@ -27,18 +27,18 @@ public class _04_ElseIf_230328 {
         주문이 완료됐습니다.
         */
 
-        // 2. else가 생략된 경우
+        // 2. else블럭이 생략된 경우
         caffeLatte = false;
         caffeMocha = false;
         matchaFrappuccino = false;
 
-        if (caffeLatte) { // false -> else if 이동
+        if (caffeLatte) { // false
             System.out.println("카페 라떼를 주문하셨습니다.");
             System.out.println("음료를 제조하고 있습니다. #1");
-        } else if (caffeMocha) { // false -> else if 이동
+        } else if (caffeMocha) { // false
             System.out.println("카페 모카를 주문하셨습니다.");
             System.out.println("음료를 제조하고 있습니다. #2");
-        } else if (matchaFrappuccino) { // false -> 종료
+        } else if (matchaFrappuccino) { // false
             System.out.println("말차 프라푸치노를 주문하셨습니다.");
             System.out.println("음료를 제조하고 있습니다. #3");
         }
