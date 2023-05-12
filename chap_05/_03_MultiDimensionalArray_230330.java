a1bde29cebbf84bb9fdb297401241099dfc3b3e9
Jisu Lee <jisulee.shsf@gmail.com>
2023-05-13 04:02:31 +0900

    Update _03_MultiDimensionalArray_230330.java

--- chap_05/_03_MultiDimensionalArray_230330.java
+++ chap_05/_03_MultiDimensionalArray_230330.java
@@ -6,7 +6,6 @@ public class _03_MultiDimensionalArray_230330 {
         String[] seatsA = new String[] {"a1", "a2", "a3", "a4", "a5"};
         String[] seatsB = new String[] {"b1", "b2", "b3", "b4", "b5"};
         String[] seatsC = new String[] {"c1", "c2", "c3", "c4", "c5"};
-        System.out.println(seatsB[2]); // b3
 
         // 2-1. 크기가 같은 2차원 배열
         String[][] seats1 = new String[][] {
@@ -22,6 +21,7 @@ public class _03_MultiDimensionalArray_230330 {
                 {"b1", "b2", "b3", "b4"},
                 {"c1", "c2", "c3", "c4", "c5"}
         };
-        System.out.println(seats2[0][2]); // a3
+        System.out.println(seats2[1][2]); // b3
+        System.out.println(seats2[1][4]); // error(ArrayIndexOutOfBoundsException)
     }
 }
