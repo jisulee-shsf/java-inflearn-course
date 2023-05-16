e8e5bf907607443f454e361c885d9d34e244136c
Jisu Lee <jisulee.shsf@gmail.com>
2023-05-16 22:35:33 +0900

    Update _02_Parameter_230330.java

--- chap_06/_02_Parameter_230330.java
+++ chap_06/_02_Parameter_230330.java
@@ -1,7 +1,7 @@
 package chap_06;
 
 public class _02_Parameter_230330 {
-    // 1. 메서드 정의(파라미터 포함)
+    // 1. 메서드 정의
     public static void power(int number) {
         int result1 = number * number;
         System.out.println(number + "의 2승은 " + result1 + "입니다.");
@@ -17,10 +17,18 @@ public class _02_Parameter_230330 {
 
     // 2. 메서드 호출
     public static void main(String[] args) {
-        power(2); // 2의 2승은 4입니다.
-        power(4); // 4의 2승은 16입니다.
+        power(2);
+        power(4);
+        /*
+        2의 2승은 4입니다.
+        4의 2승은 16입니다.
+        */
 
-        powerByExp(2, 3); // 1 * 2, 2 * 2, 4 * 2 = 8 -> 2의 3승은 8입니다.
-        powerByExp(3, 4); // 1 * 3, 3 * 3, 9 * 3, 27 * 3 = 81 -> 3의 4승은 81입니다.
+        powerByExp(2, 3);
+        powerByExp(3, 4);
+        /*
+        2의 3승은 8입니다.
+        3의 4승은 81입니다.
+        */
     }
 }
