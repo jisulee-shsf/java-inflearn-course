2b5cc87c30b3956a873ba61ab1026e7d7d3c6ce0
Jisu Lee <jisulee.shsf@gmail.com>
2023-05-17 21:34:26 +0900

    Update _03_Return_230330.java

--- chap_06/_03_Return_230330.java
+++ chap_06/_03_Return_230330.java
@@ -2,6 +2,9 @@ package chap_06;
 
 public class _03_Return_230330 {
     // 1. 메서드 정의
+    public static String getNumber() {
+        return "02-0000-0000";
+    }
     public static String getAddress() {
         String address = "서울시 중구 000";
         return address;
@@ -12,18 +15,20 @@ public class _03_Return_230330 {
         return zipCode;
     }
 
-    public static String getNumber() {
-        return "02-0000-0000";
-    }
-
     // 2. 메서드 호출
     public static void main(String[] args) {
-        String officeAddress = getAddress(); // 반환값을 저장하기 위한 변수 정의
-        System.out.println("사무실 주소: " + officeAddress); // 사무실 주소: 서울시 중구 000
+        String officeNumber = getNumber();
+        System.out.println("사무실 전화번호: " + officeNumber);
 
-        int officeZipCode = getZipCode(); // 반환값을 저장하기 위한 변수 정의
-        System.out.println("사무실 우편번호: " + officeZipCode); // 사무실 우편번호: 12345
+        String officeAddress = getAddress();
+        System.out.println("사무실 주소: " + officeAddress);
 
-        System.out.println("사무실 전화번호: " + getNumber()); // 사무실 전화번호: 02-0000-0000
+        int officeZipCode = getZipCode();
+        System.out.println("사무실 우편번호: " + officeZipCode);
+        /*
+        사무실 전화번호: 02-0000-0000
+        사무실 주소: 서울시 중구 000
+        사무실 우편번호: 12345
+        */
     }
 }
