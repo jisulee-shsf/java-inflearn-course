86ff788d696ab0e7a53e6bb4bf1385caf7bf1583
Jisu Lee <jisulee.shsf@gmail.com>
2023-05-22 02:13:07 +0900

    Update _02_InstanceVariable_230401.java

--- chap_07/_02_InstanceVariable_230401.java
+++ chap_07/_02_InstanceVariable_230401.java
@@ -3,29 +3,47 @@ package chap_07;
 public class _02_InstanceVariable_230401 {
     public static void main(String[] args) {
         // 1. 클래스 객체 생성
-        Phone p1 = new Phone();
-        Phone p2 = new Phone();
+        Phone2 p1 = new Phone2();
+        Phone2 p2 = new Phone2();
 
-        // 2. 인스턴스 변숫값 설정
+        // 2. 인스턴스 변수에 값 저장 및 출력
         p1.modelName = "자바폰1";
         p1.modelType = "프로";
         p1.modelCapacity = 128;
         p1.modelPrice = 1000000;
 
+        System.out.println(p1.modelName);
+        System.out.println(p1.modelType);
+        System.out.println(p1.modelCapacity);
+        System.out.println(p1.modelPrice);
+        /*
+        자바폰1
+        프로
+        128
+        1000000
+        */
+
         p2.modelName = "자바폰2";
         p2.modelType = "프로맥스";
         p2.modelCapacity = 256;
         p2.modelPrice = 1500000;
+
+        System.out.println(p2.modelName);
+        System.out.println(p2.modelType);
+        System.out.println(p2.modelCapacity);
+        System.out.println(p2.modelPrice);
+        /*
+        자바폰2
+        프로맥스
+        256
+        1500000
+        */
     }
 }
 
-/*
-// 인스턴스 변수
-public class Phone {
-    // 인스턴스 변수 선언
-    String modelName; // 모델명
-    String modelType; // 모델 타입
-    int modelCapacity; // 모델 용량
-    int modelPrice; // 모델 가격
+class Phone2 {
+    String modelName;
+    String modelType;
+    int modelCapacity;
+    int modelPrice;
 }
-*/
