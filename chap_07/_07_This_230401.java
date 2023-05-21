6cc4a7f7502ea78de784de229f5bf09431461707
Jisu Lee <jisulee.shsf@gmail.com>
2023-05-22 05:07:40 +0900

    Update _07_This_230401.java

--- chap_07/_07_This_230401.java
+++ chap_07/_07_This_230401.java
@@ -3,28 +3,21 @@ package chap_07;
 public class _07_This_230401 {
     public static void main(String[] args) {
         // 1. 클래스 객체 생성
-        Phone p = new Phone();
+        Phone7 p = new Phone7();
 
-        // 2. 인스턴스 변숫값 설정
+        // 2. 인스턴스 변수에 값 저장
         p.modelType = "프로";
 
-        // 3-1. 지역 변수 += 지역 변수) 메서드 호출
+        // 3. 메서드 호출
         p.appendModelType("맥스");
-        System.out.println(p.modelType); // 맥스
-
-        // 3-2. 인스턴스 변수 += 지역 변수) 메서드 호출
-        p.appendModelType("맥스");
-        System.out.println(p.modelType); // 프로맥스
+        System.out.println(p.modelType); // 프로 -> 프로맥스
     }
 }
 
-/*
-// 참조 변수 this
-public class Phone {
-    // 메서드 정의
+class Phone7 {
+    String modelType;
+
     void appendModelType(String modelType) {
-        // modelType += modelType; 지역 변수 += 지역 변수
         this.modelType += modelType; // 인스턴스 변수 += 지역 변수
     }
 }
-*/
