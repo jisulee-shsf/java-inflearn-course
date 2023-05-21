6fc73dde6df611b19cc994d8552e08691c83b6a1
Jisu Lee <jisulee.shsf@gmail.com>
2023-05-21 16:31:47 +0900

    Update _01_Class_230401.java

--- chap_07/_01_Class_230401.java
+++ chap_07/_01_Class_230401.java
@@ -2,16 +2,14 @@ package chap_07;
 
 public class _01_Class_230401 {
     public static void main(String[] args) {
-        // 1. 기존 생성 방식
+        // 1. 기존 데이터 처리
         String modelName1 = "자바폰1";
         String modelType1 = "프로";
-        
         int modelCapacity1 = 128;
         int modelPrice1 = 1000000;
         
         String modelName2 = "자바폰2";
         String modelType2 = "프로맥스";
-        
         int modelCapacity2 = 256;
         int modelPrice2 = 1500000;
         // ...
