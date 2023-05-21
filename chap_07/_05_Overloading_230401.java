7b8b5c736c6d0811c1915d414af5991d34e1706f
Jisu Lee <jisulee.shsf@gmail.com>
2023-05-22 04:53:03 +0900

    Update _05_Overloading_230401.java

--- chap_07/_05_Overloading_230401.java
+++ chap_07/_05_Overloading_230401.java
@@ -3,20 +3,16 @@ package chap_07;
 public class _05_Overloading_230401 {
     public static void main(String[] args) {
         // 1. 클래스 객체 생성
-        Phone p = new Phone();
+        Phone5 p = new Phone5();
 
-        // 2. 인스턴스 변숫값 설정
-        p.modelName = "자바폰";
-
-        // 3-1. 오버로딩) 메서드1 호출
+        // 2. 메서드 호출
         p.safety(true, true);
         /*
-        안전 기능이 시작합니다.
-        긴급 구조 요청 기능을 시작합니다.
+        안전 기능을 시작합니다.
+        긴급 구조 요청 기능을 시작합니다
         충돌 감지 기능을 시작합니다.
         */
 
-        // 3-2. 오버로딩) 메서드2 호출
         p.safety();
         /*
         안전 기능이 시작합니다.
@@ -26,13 +22,11 @@ public class _05_Overloading_230401 {
     }
 }
 
-/*
-// 오버로딩
-public class Phone {
-    // 1. 메서드1
-    void safety(boolean useEmergencySos, boolean useCrashDetection) {
+class Phone5 {
+    // 메서드 오버로딩
+    void safety(boolean useEmergencySOS, boolean useCrashDetection) {
         System.out.println("안전 기능을 시작합니다.");
-        if (useEmergencySos) {
+        if (useEmergencySOS) {
             System.out.println("긴급 구조 요청 기능을 시작합니다");
         }
         if (useCrashDetection) {
@@ -41,9 +35,7 @@ public class Phone {
         System.out.println();
     }
 
-    // 2. 메서드2
     void safety() {
-        safety(true, true); // 기본값 설정
+        safety(true, true);
     }
 }
-*/
