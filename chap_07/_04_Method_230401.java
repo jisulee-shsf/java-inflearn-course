e4711f9de7f2c6f359cfd379d99a89a66acccc7a
Jisu Lee <jisulee.shsf@gmail.com>
2023-05-22 03:34:18 +0900

    Update _04_Method_230401.java

--- chap_07/_04_Method_230401.java
+++ chap_07/_04_Method_230401.java
@@ -1,26 +1,24 @@
 package chap_07;
 
-public class
-_04_Method_230401 {
+public class _04_Method_230401 {
     public static void main(String[] args) {
         // 1. 클래스 객체 생성
-        Phone p = new Phone();
+        Phone4 p = new Phone4();
 
-        // 2. 인스턴스 변숫값 설정
-        p.modelName = "자바폰";
-        System.out.println(p.modelName); // 자바폰
-
-        // 3. 메서드 호출
-        p.canCrashDetection1(); // 충돌이 감지되었습니다.
-        p.canCrashDetection2(1); // 충돌이 감지되었습니다.
+        // 2. 메서드 호출
+        p.canCrashDetection1();
+        p.canCrashDetection2(1);
         int objectCount = p.canCrashDetection3(1);
-        System.out.println(p.modelName + "에 충돌이 감지된 물체 개수: " + objectCount + "개"); // 자바폰에 충돌이 감지된 물체 개수: 10개
+        System.out.println("충돌이 감지된 물체 개수: " + objectCount + "개");
+        /*
+        충돌이 감지되었습니다.
+        충돌이 감지되었습니다.
+        충돌이 감지된 물체 개수: 10개
+        */
     }
 }
 
-/*
-// 메서드
-public class Phone {
+class Phone4 {
     // 1. 기본 메서드 정의
     void canCrashDetection1() {
         System.out.println("충돌이 감지되었습니다.");
@@ -39,9 +37,8 @@ public class Phone {
     int canCrashDetection3(int number) {
         if (number == 1) { // 충돌이 감지된 경우
             return 10; // 충돌 감지 물체 개수
-        } else { // 이외의 경우
+        } else {
             return 0; // 충돌 감지 물체 없음
         }
     }
 }
-*/
