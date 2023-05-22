39a2e0ec66578c6fea25d75e93b79a36fcb8085c
Jisu Lee <jisulee.shsf@gmail.com>
2023-05-22 12:19:07 +0900

    Update _10_AccessModifier_230402.java

--- chap_07/_10_AccessModifier_230402.java
+++ chap_07/_10_AccessModifier_230402.java
@@ -6,28 +6,26 @@ public class _10_AccessModifier_230402 {
         PhoneUpdated pu = new PhoneUpdated();
 
         /*
-        // 2. 인스턴스 변숫값 설정
+        // 2-1. private 접근 제어자 사용 전
         pu.modelPrice = 1000000;
         System.out.println(pu.modelPrice); // 1000000
         pu.modelPrice = -1000000; // 잘못된 값으로 설정될 수 있음
         System.out.println(pu.modelPrice); // -1000000
 
-        // 3. public -> private) 접근 제어자 변경 및 메서드 호출
+        // 2-2. private 접근 제어자 사용 후
         pu.modelPrice = 1000000;
         System.out.println(pu.modelPrice); // java: modelPrice has private access in chap_07.PhoneUpdated
         pu.modelPrice = -1000000;
         System.out.println(pu.modelPrice); // java: modelPrice has private access in chap_07.PhoneUpdated
         */
 
-        // 4. getter & setter) 메서드 호출
-        pu.setModelPrice(1500000);
+        // 3. getter & setter) 메서드 호출
+        pu.setModelPrice(-1500000);
         System.out.println(pu.getModelPrice()); // 1500000
     }
 }
 
-/*
-// 접근 제어자
-public class PhoneUpdated {
+class PhoneUpdated {
     // 1. private) 인스턴스 변수 선언
     private int modelPrice; // 모델 가격
 
@@ -36,7 +34,10 @@ public class PhoneUpdated {
         return modelPrice;
     }
     public void setModelPrice(int modelPrice) {
-        this.modelPrice = modelPrice;
+        if (modelPrice < 1500000) {
+            this.modelPrice = 1500000;
+        } else {
+            this.modelPrice = modelPrice;
+        }
     }
 }
-*/
