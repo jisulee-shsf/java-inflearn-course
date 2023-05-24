cf3c6c46235171d4928d56fca38ff7d9a30f6965
Jisu Lee <jisulee.shsf@gmail.com>
2023-05-24 17:54:46 +0900

    Update _01_AbstractClass_230406.java

--- chap_08/_01_AbstractClass_230406.java
+++ chap_08/_01_AbstractClass_230406.java
@@ -1,21 +1,45 @@
 package chap_08;
 
-import chap_08.classes.*;
-
 public class _01_AbstractClass_230406 {
     public static void main(String[] args) {
-        // 1. 클래스 객체 생성 및 메서드 호출
-        AbstractClassB v1 = new AbstractClassB();
-        v1.findSth(); // AbstractClassB에서 findSth을 시작합니다.
+        // 1. 클래스 객체 생성 & 메서드 호출
+        AbstractClassB ac1 = new AbstractClassB();
+        ac1.findSth();
+        AbstractClassC ac2 = new AbstractClassC();
+        ac2.findSth();
+        /*
+        AbstractClassB에서 findSth을 시작합니다.
+        AbstractClassC에서 findSth을 시작합니다.
+        */
 
-        AbstractClassC v2 = new AbstractClassC();
-        v2.findSth(); // AbstractClassC에서 findSth을 시작합니다.
+        // 2. 다형성) 클래스 객체 생성 & 메서드 호출
+        AbstractClassA ac3 = new AbstractClassB();
+        ac3.findSth();
+        AbstractClassA ac4 = new AbstractClassC();
+        ac4.findSth();
+        /*
+        AbstractClassB에서 findSth을 시작합니다.
+        AbstractClassC에서 findSth을 시작합니다.
+        */
+    }
+}
 
-        // 2. 다형성) 클래스 객체 생성 및 메서드 호출
-        AbstractClassA v3 = new AbstractClassB();
-        v3.findSth(); // AbstractClassB에서 findSth을 시작합니다.
+abstract class AbstractClassA { // 추상 클래스
+    /* 어떤 기능을 수행할 목적으로 작성했는지 설명 삽입 */
+    public abstract void findSth(); // 추상 메서드
+}
 
-        AbstractClassA v4 = new AbstractClassC();
-        v4.findSth(); // AbstractClassC에서 findSth을 시작합니다.
+class AbstractClassB extends AbstractClassA {
+    @Override
+    public void findSth() { // 추상 메서드 구현
+        System.out.println("AbstractClassB에서 findSth을 시작합니다.");
+    }
+}
+
+class AbstractClassC extends AbstractClassA {
+    @Override
+    public void findSth() { // 추상 메서드 구현
+        System.out.println("AbstractClassC에서 findSth을 시작합니다.");
+
     }
 }
