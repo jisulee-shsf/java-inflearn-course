8d385ec38eaa972d4d90dd9bf6745570c8765739
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2023-05-27 05:27:24 +0900

    Update AbstractClass.java

--- chap_08/classes/phone/AbstractClass.java
+++ chap_08/classes/phone/AbstractClass.java
@@ -1,6 +1,7 @@
 package chap_08.classes.phone;
 
-public abstract class AbstractClass { // 추상 클래스
+// 추상 클래스
+public abstract class AbstractClass {
     // 추상 메서드
-    public abstract void findSth(); // 추상 메서드
+    public abstract void findSth();
 }
