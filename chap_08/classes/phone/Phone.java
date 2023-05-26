6372cbac6edd260d661e907eb9caafc23e66fed8
Jisu Lee <jisulee.shsf@gmail.com>
2023-05-26 20:06:03 +0900

    Update Phone.java

--- chap_08/classes/phone/Phone.java
+++ chap_08/classes/phone/Phone.java
@@ -1,9 +1,9 @@
 package chap_08.classes.phone;
 
-import chap_08.classes.checker.Checkable;
-import chap_08.classes.correcter.Correctable;
+import chap_08.classes.checker.*;
+import chap_08.classes.correcter.*;
 
-public class InterfaceB extends InterfaceA implements Checkable, Correctable {
+public class Phone extends AbstractClass implements Checkable, Correctable {
     // 1. 인터페이스 변수 선언
     private Checkable checker;
     private Correctable correcter;
@@ -20,7 +20,7 @@ public class InterfaceB extends InterfaceA implements Checkable, Correctable {
     // 3-1. 추상 클래스) 메서드 구현
     @Override
     public void findSth() {
-        System.out.println("InterfaceTestB에서 findSth을 시작합니다.");
+        System.out.println("InterfaceB에서 findSth을 시작합니다.");
     }
 
     // 3-2. 인터페이스) 메서드 구현
