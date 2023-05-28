4aef43287d9604c0484860abc3ae9bab68c4a4ff
Jisu Lee <jisulee.shsf@gmail.com>
2023-05-28 20:22:02 +0900

    Update _02_GenericClass_230412.java

--- chap_09/_02_GenericClass_230412.java
+++ chap_09/_02_GenericClass_230412.java
@@ -5,39 +5,41 @@ import chap_09.order.*;
 
 public class _02_GenericClass_230412 {
     public static void main(String[] args) {
-        // 1. 기존 클래스
-        OrderByNumber trackByNumber = new OrderByNumber(123);
-        trackByNumber.printOrderInfo();
-        OrderByName trackByName = new OrderByName("홍길동1");
-        trackByName.printOrderInfo();
+        // 제네릭 클래스
+        OrderByNumber orderByNumber = new OrderByNumber(123);
+        orderByNumber.printOrderInfo();
+
+        OrderByName orderByName = new OrderByName("홍길동1");
+        orderByName.printOrderInfo();
         /*
         주문 정보: 123
         주문 정보: 홍길동1
         */
 
-        // 2. 제네릭 클래스
-        Order tracking1 = new Order<>(345);
-        tracking1.printOrderInfo();
-        Order tracking2 = new Order<>("홍길동2");
-        tracking2.printOrderInfo();
+        OrderByAnyType orderByAnyType1 = new OrderByAnyType<>(345);
+        orderByAnyType1.printOrderInfo();
+
+        OrderByAnyType orderByAnyType2 = new OrderByAnyType<>("홍길동2");
+        orderByAnyType2.printOrderInfo();
         /*
         주문 정보: 345
         주문 정보: 홍길동2
         */
 
+        // Menu menu = new Menu("까르보나라"); -> Menu 클래스로부터 menu 객체 생성
+        // OrderByUser<Menu> orderByUser1 = new OrderByUser<>(menu); -> OrderByUser 생성자 인자로 전달
         OrderByUser<Menu> orderByUser1 = new OrderByUser<>(new Menu("까르보나라"));
         orderByUser1.printOrderInfo();
+
+        OrderByUser<Menu> orderByUser2 = new OrderByUser<>(new SpecialMenu("안심 스테이크"));
+        // OrderByUser<SpecialMenu> orderByUser2 = new OrderByUser<>(new SpecialMenu("안심 스테이크"));
+        orderByUser2.printOrderInfo();
         /*
         주문 정보: 까르보나라
         까르보나라 주문이 입력됐습니다.
-        */
 
-        OrderByUser<Menu> orderByUser2 = new OrderByUser<>(new SpecialMenu("안심 스테이크"));
-        // OrderByUser<SpecialMenu> orderByUser2 = new OrderByUser<>(new SpecialMenu("스테이크"));
-        orderByUser2.printOrderInfo();
-        /*
-        주문 정보: 스페셜 메뉴 스테이크
-        스페셜 메뉴 스테이크 주문이 입력됐습니다.
+        주문 정보: 스페셜 메뉴 안심 스테이크
+        스페셜 메뉴 안심 스테이크 주문이 입력됐습니다.
         */
     }
 }
