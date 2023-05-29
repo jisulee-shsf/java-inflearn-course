a60903a3030f97bcfede14aa03b0e68ea86abed8
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2023-05-29 21:35:13 +0900

    Update _04_ArrayList_230413.java

--- chap_09/_04_ArrayList_230413.java
+++ chap_09/_04_ArrayList_230413.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 
 public class _04_ArrayList_230413 {
     public static void main(String[] args) {
-        // Array List(중복 허용 & 순서 보장)
+        // Array List(데이터 저장 순서 유지 / 중복 허용)
         ArrayList<String> list = new ArrayList<>();
 
         // 1. add & get
