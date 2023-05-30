e3eefafb656beecead82b9355ab65fc300aed141
Jisu Lee <jisulee.shsf@gmail.com>
2023-05-30 18:42:43 +0900

    Update _07_HashMap_230414.java

--- chap_09/_07_HashMap_230414.java
+++ chap_09/_07_HashMap_230414.java
@@ -5,7 +5,7 @@ import java.util.LinkedHashMap;
 
 public class _07_HashMap_230414 {
     public static void main(String[] args) {
-        // HashMap(중복 허용 불가 & 순서 보장 불가)
+        // HashMap
         HashMap<String, Integer> hashmap = new HashMap<>();
 
         // 1. put
@@ -30,8 +30,8 @@ public class _07_HashMap_230414 {
         */
 
         // 3. KeySet & values
-        for (String s : hashmap.keySet()) {
-            System.out.println(s);
+        for (String key : hashmap.keySet()) {
+            System.out.println(key);
         }
         /*
         다길동
@@ -41,8 +41,8 @@ public class _07_HashMap_230414 {
         라길동
         */
 
-        for (int i : hashmap.values()) {
-            System.out.println(i);
+        for (int value : hashmap.values()) {
+            System.out.println(value);
         }
         /*
         30
@@ -52,8 +52,8 @@ public class _07_HashMap_230414 {
         40
         */
 
-        for (String s : hashmap.keySet()) {
-            System.out.println("key: " + s + "\tvalue: " + hashmap.get(s));
+        for (String key : hashmap.keySet()) {
+            System.out.println("key: " + key + "\tvalue: " + hashmap.get(key));
         }
         /*
         key: 다길동	value: 30
@@ -63,10 +63,29 @@ public class _07_HashMap_230414 {
         key: 라길동	value: 40
         */
 
-        // 4. containsKey
+        // 4. remove
+        System.out.println(hashmap.size());
+        hashmap.remove("마길동");
+        System.out.println(hashmap.size());
+        /*
+        5
+        4
+        */
+
+        for (String key : hashmap.keySet()) {
+            System.out.println("key: " + key + "\tvalue: " + hashmap.get(key));
+        }
+        /*
+        key: 다길동	value: 30
+        key: 나길동	value: 20
+        key: 가길동	value: 10
+        key: 라길동	value: 40
+        */
+
+        // 5. containsKey
         if (hashmap.containsKey("가길동")) {
             int i = hashmap.get("가길동");
-            hashmap.put("가길동", ++i); // 최종값 업데이트
+            hashmap.put("가길동", ++i); // 10 -> 11
             System.out.println("key: 가길동\tvalue: " + hashmap.get("가길동"));
         } else {
             hashmap.put("가길동", 1);
@@ -74,15 +93,6 @@ public class _07_HashMap_230414 {
         }
         /*
         key: 가길동	value: 11
-        */
-
-        // 5. remove
-        System.out.println(hashmap.size());
-        hashmap.remove("마길동");
-        System.out.println(hashmap.size());
-        /*
-        5
-        4
         */
 
         // 6. clear
@@ -96,7 +106,7 @@ public class _07_HashMap_230414 {
         이름 정보 없음
         */
 
-        // LinkedHashMap(중복 허용 불가 & 순서 보장)
+        // LinkedHashMap(데이터 저장 순서 유지)
         HashMap<String, Integer> linkedHashMap = new LinkedHashMap<>();
 
         // 1. put
@@ -108,9 +118,31 @@ public class _07_HashMap_230414 {
         linkedHashMap.put("마길동", 60);
         linkedHashMap.put("마길동", 70);
 
-        // 2. get & keySet
-        for (String s : linkedHashMap.keySet()) {
-            System.out.println("key: " + s + "\tvalue: " + linkedHashMap.get(s));
+        // 2. KeySet & values
+        for (String key : linkedHashMap.keySet()) {
+            System.out.println(key);
+        }
+        /*
+        가길동
+        나길동
+        다길동
+        라길동
+        마길동
+        */
+
+        for (int value : linkedHashMap.values()) {
+            System.out.println(value);
+        }
+        /*
+        10
+        20
+        30
+        40
+        70
+        */
+
+        for (String key : linkedHashMap.keySet()) {
+            System.out.println("key: " + key + "\tvalue: " + linkedHashMap.get(key));
         }
         /*
         key: 가길동	value: 10
