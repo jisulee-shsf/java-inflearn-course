431622df86ff11127f0d2902f3f51d6b1405a36a
Jisu Lee <jisulee.shsf@gmail.com>
2023-06-04 22:59:25 +0900

    Update _03_Throw_230426.java

--- chap_11/_03_Throw_230426.java
+++ chap_11/_03_Throw_230426.java
@@ -5,7 +5,7 @@ public class _03_Throw_230426 {
         try {
             int code = 1111;
             if (code != 1234) {
-                throw new Exception("코드 입력에 오류가 발생했습니다.");
+                throw new Exception("입력하신 코드에 오류가 발생했습니다.");
             } else {
                 System.out.println("프로그램을 실행합니다.");
             }
@@ -14,8 +14,8 @@ public class _03_Throw_230426 {
         };
         System.out.println("프로그램 종료");
         /*
-        java.lang.Exception: 코드 입력에 오류가 발생했습니다.
-	    at chap_11._03_Throw_230426.main(_03_Throw_230426.java:8)
+        java.lang.Exception: 입력하신 코드에 오류가 발생했습니다.
+	        at chap_11._03_Throw_230426.main(_03_Throw_230426.java:8)
         프로그램 종료
         */
     }
