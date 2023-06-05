c7d73d2f07340ca1268d561d5661512d0a3854b0
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2023-06-06 00:00:31 +0900

    Update _01_Thread_230428.java

--- chap_12/_01_Thread_230428.java
+++ chap_12/_01_Thread_230428.java
@@ -8,42 +8,42 @@ public class _01_Thread_230428 {
         detectThread.run();
         detect();
         /*
-        detecter1) 1번째 오류 감지 -> Thread
-        detecter1) 3번째 오류 감지 -> Thread
-        detecter1) 5번째 오류 감지 -> Thread
-        detecter1) 7번째 오류 감지 -> Thread
-        detecter1) 9번째 오류 감지 -> Thread
-        detecter1) 전체 감지 완료 -> Thread
-        detecter2) 2번째 오류 감지
-        detecter2) 4번째 오류 감지
-        detecter2) 6번째 오류 감지
-        detecter2) 8번째 오류 감지
-        detecter2) 10번째 오류 감지
-        detecter2) 전체 감지 완료
+        detector1) 1번째 오류 감지 -> Thread
+        detector1) 3번째 오류 감지 -> Thread
+        detector1) 5번째 오류 감지 -> Thread
+        detector1) 7번째 오류 감지 -> Thread
+        detector1) 9번째 오류 감지 -> Thread
+        detector1) 전체 감지 완료 -> Thread
+        detector2) 2번째 오류 감지
+        detector2) 4번째 오류 감지
+        detector2) 6번째 오류 감지
+        detector2) 8번째 오류 감지
+        detector2) 10번째 오류 감지
+        detector2) 전체 감지 완료
         */
 
         detectThread.start();
         detect();
         /*
-        detecter2) 2번째 오류 감지
-        detecter2) 4번째 오류 감지
-        detecter1) 1번째 오류 감지 -> Thread
-        detecter1) 3번째 오류 감지 -> Thread
-        detecter1) 5번째 오류 감지 -> Thread
-        detecter1) 7번째 오류 감지 -> Thread
-        detecter2) 6번째 오류 감지
-        detecter1) 9번째 오류 감지 -> Thread
-        detecter1) 전체 감지 완료 -> Thread
-        detecter2) 8번째 오류 감지
-        detecter2) 10번째 오류 감지
-        detecter2) 전체 감지 완료
+        detector2) 2번째 오류 감지
+        detector2) 4번째 오류 감지
+        detector1) 1번째 오류 감지 -> Thread
+        detector1) 3번째 오류 감지 -> Thread
+        detector1) 5번째 오류 감지 -> Thread
+        detector1) 7번째 오류 감지 -> Thread
+        detector2) 6번째 오류 감지
+        detector1) 9번째 오류 감지 -> Thread
+        detector1) 전체 감지 완료 -> Thread
+        detector2) 8번째 오류 감지
+        detector2) 10번째 오류 감지
+        detector2) 전체 감지 완료
         */
     }
 
     public static void detect() {
         for (int i = 2; i <= 10; i += 2) {  // 2, 4, 6, 8, 10
-            System.out.println("detecter2) " + i + "번째 오류 감지");
+            System.out.println("detector2) " + i + "번째 오류 감지");
         }
-        System.out.println("detecter2) 전체 감지 완료");
+        System.out.println("detector2) 전체 감지 완료");
     }
 }
