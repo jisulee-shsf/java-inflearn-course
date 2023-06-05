48efd756c1784e748ca8df420332908cccdc9560
Jisu Lee <jisulee.shsf@gmail.com>
2023-06-05 20:00:32 +0900

    Update _05_TryWithResources_230426.java

--- chap_11/_05_TryWithResources_230426.java
+++ chap_11/_05_TryWithResources_230426.java
@@ -2,10 +2,10 @@ package chap_11;
 
 public class _05_TryWithResources_230426 {
     public static void main(String[] args) {
-        // 1. sdf
-        fileWriter fw1 = null;
+        // 1. close() 메서드 기재 후 호출
+        FileWriter fw1 = null;
         try {
-            fw1 = new fileWriter();
+            fw1 = new FileWriter();
             fw1.write("첫 번째 내용을 입력합니다.");
         } catch (Exception e) {
             e.printStackTrace();
@@ -22,7 +22,8 @@ public class _05_TryWithResources_230426 {
         파일을 닫습니다.
         */
 
-        try (fileWriter fw2 = new fileWriter()) {
+        // 2. close() 메서드 자동 호출
+        try (FileWriter fw2 = new FileWriter()) { // AutoCloseable 인터페이스를 구현한 클래스의 객체
             fw2.write("두 번째 내용을 입력합니다.");
         } catch (Exception e) {
             e.printStackTrace();
@@ -35,7 +36,7 @@ public class _05_TryWithResources_230426 {
     }
 }
 
-class fileWriter implements AutoCloseable {
+class FileWriter implements AutoCloseable {
 
     @Override
     public void close() throws Exception {
