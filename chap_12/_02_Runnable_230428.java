0ed3807dd4f8f2c3c3d366a01f88309a5f529e43
Jisu Lee <jisulee.shsf@gmail.com>
2023-06-06 00:03:41 +0900

    Update _02_Runnable_230428.java

--- chap_12/_02_Runnable_230428.java
+++ chap_12/_02_Runnable_230428.java
@@ -9,25 +9,25 @@ public class _02_Runnable_230428 {
         thread.start();
         detect();
         /*
-        detecter1) 1번째 오류 감지 -> Runnable
-        detecter2) 2번째 오류 감지
-        detecter1) 3번째 오류 감지 -> Runnable
-        detecter2) 4번째 오류 감지
-        detecter1) 5번째 오류 감지 -> Runnable
-        detecter1) 7번째 오류 감지 -> Runnable
-        detecter1) 9번째 오류 감지 -> Runnable
-        detecter1) 전체 감지 완료 -> Runnable
-        detecter2) 6번째 오류 감지
-        detecter2) 8번째 오류 감지
-        detecter2) 10번째 오류 감지
-        detecter2) 전체 감지 완료
+        detector1) 1번째 오류 감지 -> Runnable
+        detector2) 2번째 오류 감지
+        detector1) 3번째 오류 감지 -> Runnable
+        detector2) 4번째 오류 감지
+        detector1) 5번째 오류 감지 -> Runnable
+        detector1) 7번째 오류 감지 -> Runnable
+        detector1) 9번째 오류 감지 -> Runnable
+        detector1) 전체 감지 완료 -> Runnable
+        detector2) 6번째 오류 감지
+        detector2) 8번째 오류 감지
+        detector2) 10번째 오류 감지
+        detector2) 전체 감지 완료
         */
     }
 
     public static void detect() {
         for (int i = 2; i <= 10; i += 2) {  // 2, 4, 6, 8, 10
-            System.out.println("detecter2) " + i + "번째 오류 감지");
+            System.out.println("detector2) " + i + "번째 오류 감지");
         }
-        System.out.println("detecter2) 전체 감지 완료");
+        System.out.println("detector2) 전체 감지 완료");
     }
 }
