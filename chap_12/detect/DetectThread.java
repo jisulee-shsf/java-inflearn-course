277398a91d6bb8749dbf448dd4b47d334a5567bf
Jisu Lee <jisulee.shsf@gmail.com>
2023-06-05 21:28:11 +0900

    Update DetectThread.java

--- chap_12/detect/DetectThread.java
+++ chap_12/detect/DetectThread.java
@@ -2,9 +2,9 @@ package chap_12.detect;
 
 public class DetectThread extends Thread {
     public void run() {
-        for (int i = 2; i <= 5; i += 2) {
-            System.out.println("detect2) " + i + "번째 오류 감지 -> Thread");
+        for (int i = 2; i <= 10; i += 2) { // 2, 4, 6, 8, 10
+            System.out.println("detecter1) " + i + "번째 오류 감지 -> Thread");
         }
-        System.out.println("detect2) 전체 감지 완료 -> Thread");
+        System.out.println("detecter1) 전체 감지 완료 -> Thread");
     }
 }
