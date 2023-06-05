735045c145424422b2cc68e03d6083637181d776
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2023-06-05 23:58:12 +0900

    Update DetectRunnable.java

--- chap_12/detect/DetectRunnable.java
+++ chap_12/detect/DetectRunnable.java
@@ -4,8 +4,8 @@ public class DetectRunnable implements Runnable {
     @Override
     public void run() {
         for (int i = 1; i <= 10; i += 2) { // 1, 3, 5, 7, 9
-            System.out.println("detecter1) " + i + "번째 오류 감지 -> Runnable");
+            System.out.println("detector1) " + i + "번째 오류 감지 -> Runnable");
         }
-        System.out.println("detecter1) 전체 감지 완료 -> Runnable");
+        System.out.println("detector1) 전체 감지 완료 -> Runnable");
     }
 }
