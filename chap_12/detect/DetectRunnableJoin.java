bc8b72581d2df94b9b464048c4262b879ae1ac6d
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2023-06-05 23:58:25 +0900

    Update DetectRunnableJoin.java

--- chap_12/detect/DetectRunnableJoin.java
+++ chap_12/detect/DetectRunnableJoin.java
@@ -4,13 +4,13 @@ public class DetectRunnableJoin implements Runnable {
     @Override
     public void run() {
         for (int i = 1; i <= 10; i += 2) {
-            System.out.println("detecter1) " + i + "번째 오류 감지 -> RunnableJoin");
+            System.out.println("detector1) " + i + "번째 오류 감지 -> RunnableJoin");
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
         }
-        System.out.println("detecter1) 전체 감지 완료 -> RunnableJoin");
+        System.out.println("detector1) 전체 감지 완료 -> RunnableJoin");
     }
 }
