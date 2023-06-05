a59415a1d92ce8d518b2e1d8673e4977c574ec64
Jisu Lee <jisulee.shsf@gmail.com>
2023-06-05 20:14:35 +0900

    Update _06_CustomException_230427.java

--- chap_11/_06_CustomException_230427.java
+++ chap_11/_06_CustomException_230427.java
@@ -5,11 +5,11 @@ public class _06_CustomException_230427 {
         int code = 1111;
         try {
             if (code != 1234) {
-                throw new codeException("코드가 잘못 입력되었습니다.");
+                throw new CodeException("코드가 잘못 입력되었습니다.");
             } else {
                 System.out.println("프로그램을 실행합니다.");
             }
-        } catch (codeException e) {
+        } catch (CodeException e) {
             System.out.println("오류 확인: " + e.getMessage());
         } catch (Exception e) {
             System.out.println("프로그램을 종료합니다.");
@@ -21,8 +21,8 @@ public class _06_CustomException_230427 {
     }
 }
 
-class codeException extends Exception {
-    public codeException(String message) {
+class CodeException extends Exception {
+    public CodeException(String message) {
         super(message);
     }
 }
