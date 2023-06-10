997c5d87cc1c1d9bd8f416392669f49e759b9340
Jisu Lee <jisulee.shsf@gmail.com>
2023-06-10 17:14:22 +0900

    Update DetectRunnableJoin.java

--- chap_12/detect/DetectRunnableJoin.java
+++ chap_12/detect/DetectRunnableJoin.java
@@ -3,7 +3,7 @@ package chap_12.detect;
 public class DetectRunnableJoin implements Runnable {
     @Override
     public void run() {
-        for (int i = 1; i <= 10; i += 2) {
+        for (int i = 1; i <= 15; i += 2) {
             System.out.println("detector1) " + i + "번째 오류 감지 -> RunnableJoin");
             try {
                 Thread.sleep(1000);
